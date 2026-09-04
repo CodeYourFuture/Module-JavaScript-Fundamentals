6db1797f075bcde486859a6e0d526f039472ec14
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-09-04 17:18:40 +0100

    completed the mandatory implement file

--- Sprint-3/3-mandatory-implement/3-to-pounds.js
+++ Sprint-3/3-mandatory-implement/3-to-pounds.js
@@ -1,6 +1,27 @@
-// In Sprint-1, there is a program written in 3-mandatory-interpret/3-to-pounds.js
+// In Sprint-1, there is a program written in interpret/to-pounds.js
 
 // You will need to take this code and turn it into a reusable block of code.
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+ function toPounds(penceString){
+    const penceStringWithoutTrailingP = penceString.substring(
+  0,
+  penceString.length - 1
+);
+    const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+const pounds = paddedPenceNumberString.substring(
+  0,
+  paddedPenceNumberString.length - 2
+);
+
+const pence = paddedPenceNumberString
+  .substring(paddedPenceNumberString.length - 2)
+  .padEnd(2, "0");
+  return `£${pounds}.${pence}`
+ }
+
+
+console.log(toPounds('987p'));
+console.log(toPounds('909p'));
+console.log(toPounds('345p'));
