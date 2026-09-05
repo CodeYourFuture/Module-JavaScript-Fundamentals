729eef2c6a8c785900282de7abf5ee94e874892b
mandip sanger <mandipsanger1@gmail.com>
2026-09-05 15:36:12 +0100

    count.js

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -6,3 +6,4 @@ count = count + 1;
 // Describe what line 3 is doing, in particular focus on what = is doing
 // Line 3 is reassigning  the value of the variable count by adding 1 to its initial value.
 // The = sign is assigning the result of the expression on the right side (count + 1) to the variable count on the left side.
+//
