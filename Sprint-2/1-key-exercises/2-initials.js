c0c59e965638154902866198306794fa612abb12
HM-127BTY <mills.hugh@yahoo.com>
2026-09-12 15:36:32 +0100

    Done task for 2-initials.js

--- Sprint-2/1-key-exercises/2-initials.js
+++ Sprint-2/1-key-exercises/2-initials.js
@@ -5,6 +5,6 @@ const lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-const initials = ``;
+const initials = firstName.slice(0,1) + middleName.slice(0,1) + lastName.slice(0,1);
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
