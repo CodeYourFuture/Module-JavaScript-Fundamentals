a50c7a72892eb5c18008c8e2ecd859f5213b2475
HM-127BTY <mills.hugh@yahoo.com>
2026-09-15 17:26:08 +0100

    TypeError for 1.js Given comment and suggestion

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -2,3 +2,11 @@
 
 const age = 33;
 age = age + 1;
+
+// this gives a TypeError
+// age is already a const at the start and calling it again just as age wont work, naming it like "older" would work better:
+
+// const age = 33;
+// let older = (age + 1);
+// console.log(older)
+
