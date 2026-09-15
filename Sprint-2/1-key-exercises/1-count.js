a9d224efed09bbba4ce45e213b7e9700429283bd
chefmatthaus-hub <chef.matthaus@gmail.com>
2026-09-15 16:39:04 +0200

    Worked on 1-count.js exxplained what the = operator does and how it changes the value of variable. Also declared a variable called initials to store the first characters of each string

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -2,5 +2,6 @@ let count = 0;
 
 count = count + 1;
 
-// Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+console.log(count);
+// Line 3 is updating the value of the variable count by adding 1 to its current value. The = operator is used to assign the new value (which is the result of count + 1) back to the variable count. This means that after line 3 executes, count will hold a value that is one greater than it did before.
+
