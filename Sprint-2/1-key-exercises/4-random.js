14355acc68c88684a6cca2c23ba7ed4d1485d313
HM-127BTY <mills.hugh@yahoo.com>
2026-09-15 17:15:42 +0100

    Done the answers for 4-random.js Added // console.log(num); to be able to see code generation

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -3,7 +3,15 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
+// console.log(num);
+
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+/*
+Firstly its not running as it is showing as undefined as it is lacking something like console.log
+After adding console.log(num) and running it a few times I get 62, 45, 35, 10. 
+For what it is doing: Math.floor is the sum of a random number from 0 to 0.9999... which is then multiplied by the maxium - minimun + 1, so 100 - 1 + 1.
+    That gives a number from 0-99, then then adds the minimun which is 1, so that gives a random number from 1-100.
+*/
