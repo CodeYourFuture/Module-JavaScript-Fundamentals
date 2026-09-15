f967651400528110e461d964bb5a12dbcfb2db82
Hanna Bohlin <ha.bohlin@gmail.com>
2026-09-15 15:22:39 +0100

    Answer question in 1-count.js

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -4,3 +4,7 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// Answer: The = is an assignment operator (as opposed to == and === which are comparison operators).
+// In line 3, a new value is assigned to the variable count. This is allowed because the variable was
+// declared using let. count is reassigned to the old value of count plus 1.
