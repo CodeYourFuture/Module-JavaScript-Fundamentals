04de5cb52262cad39c84a3b69da14bf95a48db47
HM-127BTY <mills.hugh@yahoo.com>
2026-09-15 17:40:30 +0100

    Error for 2.js is ReferenceError and gave suggestion Error for 3.js is TypeError and added code that would run it.

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,10 +1,10 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-// console.log(`I was born in ${cityOfBirth}`);
-// const cityOfBirth = "Bolton";
-
-//ReferenceError is happening 
-
-const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
+const cityOfBirth = "Bolton";
+
+//ReferenceError is happening as the const is after the console.log request for it, so the order is wrong.
+
+// const cityOfBirth = "Bolton";
+// console.log(`I was born in ${cityOfBirth}`);
