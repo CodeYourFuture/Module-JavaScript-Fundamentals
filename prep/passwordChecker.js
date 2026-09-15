31d84260140c9e2f55546548360c1d91afc762f4
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-15 23:41:11 +0100

    Add else if condition to password validation logic in passwordChecker.js

--- prep/passwordChecker.js
+++ prep/passwordChecker.js
@@ -5,7 +5,9 @@ const adminPassword="override";
 if(userInput === password){
     console.log("Correct password entered");
 }
-
+else if(userInput === adminPassword){
+    console.log("Admin access granted");
+} 
 else{
     console.log("Incorrect password!");
 }
