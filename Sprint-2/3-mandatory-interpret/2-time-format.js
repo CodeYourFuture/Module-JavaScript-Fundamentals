81af5eedf9d210c754959ac8c4710e961dd25695
Hanna Bohlin <ha.bohlin@gmail.com>
2026-09-15 23:28:46 +0100

    Answer questions in 2-time-format.js

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -1,4 +1,4 @@
-const movieLength = 8784; // length of movie in seconds
+const movieLength = 145678; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -13,13 +13,43 @@ console.log(result);
 
 // a) How many variable declarations are there in this program?
 
+// 6
+
+
+
 // b) How many function calls are there?
+
+// 0
+
+
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
+// % is the remainder arithmetic operator. The expression gives the remainder after the number of seconds
+// the movie is long is divided by 60. After you have the full amount of minutes this is the amount of
+// remaining seconds that don't add up to a full minute.
+
+
+
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+
+// removing remainingSeconds from movieLength leaves a number of seconds, slightly shorter than the movie length,
+// that are an exact amount of minutes. It is divisible by 60. Dividing that number by 60 gives the exact 
+// number of minutes.
+
+
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
+// It represents the movie length as a string expressed in hours, minutes and seconds. A different name could
+// be moiveLengthDestructured, movieLengthMessage, movieLengthInHMS, movieLengthFormatted, movieLengthTimeFormat
+
+
+
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+// It technically works for all movie lengths, but the format gets weird. when a value is a single digit, you
+// would expect 05 or 00, but it just says 5 or 0 which isn't a standard way to show time. Also, when the
+// hours go above 24 you would expect it to start counting days, but the hours just add up instead as
+// you try higher values.
