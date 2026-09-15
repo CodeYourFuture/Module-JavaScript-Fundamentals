56ac4cd6e0a45e3d208a6d69929930ff4c3c1bfb
chefmatthaus-hub <chef.matthaus@gmail.com>
2026-09-15 20:11:12 +0200

    completed 2.js by swapping lines 6 and 5 so that  const decleration is before console.log

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+// Answer: because javascript runs from top to bottom, by declaring the const after running console.log the function throws an error because the decleration is done ater console.log
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
