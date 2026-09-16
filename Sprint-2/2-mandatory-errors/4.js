0c7b74938230971aa5857711c2ef93d1bbd02325
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-16 22:46:54 +0100

    Add comments explaining SyntaxError due to invalid identifier names

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -2,3 +2,6 @@ const 12HourClockTime = "8:53pm";
 const 24hourClockTime = "20:53";
 
 console.log(12HourClockTime, 24hourClockTime);
+
+//There is a SyntaxError after running the code.
+//This error occurred because the identifiers '12HourClockTime' and '24hourClockTime' both start with a digit.
