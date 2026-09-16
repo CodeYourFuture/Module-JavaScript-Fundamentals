00e5ddc609393a07f4eaad2e99828974cbed8798
Qhama Gwele <lwandogwele52@gmail.com>
2026-09-16 19:51:45 +0200

    Add JavaScript fundamentals prep exercises

---
+++ prep/comparison.js
@@ -0,0 +1,12 @@
+// 1
+console.log("hello" === "hello");
+
+// 2
+console.log("CYF" === "cyf");
+
+// 3
+const homeTown = "Newcastle";
+console.log(homeTown === "Liverpool");
+
+// 4
+console.log(42 === 42);
---
+++ prep/greeting.js
@@ -0,0 +1,2 @@
+const greeting = "hello there";
+console.log(greeting);
