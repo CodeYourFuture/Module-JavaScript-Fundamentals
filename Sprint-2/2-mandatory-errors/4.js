50f9d8cd3c8a682e99a345b6bcd6746ab47527e8
Hanna Bohlin <ha.bohlin@gmail.com>
2026-09-16 21:40:33 +0100

    Explained the error message in 4.js

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,4 +1,6 @@
 const twelveHourClockTime = "8:53pm";
 const twentyfourHourClockTime = "20:53";
 
+// Answer: I got the error message SyntaxError: Invalid or unexpected token
+// There were arrows pointing at "12" in 12HourClockTime. That is because
 // variable names cannot start with numbers in javascript. I changed them to start with letters.
