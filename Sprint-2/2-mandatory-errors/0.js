0175ffcefc5e9f4d9bdb0446581891a552132e23
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-16 21:53:54 +0100

    Wrap instructional lines in multi-line comment and add solution explanation

--- Sprint-2/2-mandatory-errors/0.js
+++ Sprint-2/2-mandatory-errors/0.js
@@ -1,2 +1,5 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+/*This is just an instruction for the first activity - but it is just for human consumption
+We don't want the computer to run these 2 lines - how can we solve this problem?*/
+
+//Solution
+//This problem was solved by wrapping the statement in a multi-line comment tag. This way the lines are stopped from executing.
