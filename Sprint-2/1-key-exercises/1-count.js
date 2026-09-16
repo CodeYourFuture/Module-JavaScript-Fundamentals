ac61856be3bc518b275806b5b0bb88b82f8cff43
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-16 09:37:35 +0100

    Add detailed comments explaining count variable increment in 1-count.js

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -5,3 +5,8 @@ count = count + 1;
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
 console.log(count);
+
+//Line 3 is reading the current value of <count>, which is <0>, and computing <count + 1>, which turns out to be 1.
+//It also assigns the new value <1> to the variable <count>, overwriting the initial value <0>.
+//The assignment operator "=" means, take the value on the right, and store it in the variable on the left.
+//Console.log(count) prints out the result of Line 3.
