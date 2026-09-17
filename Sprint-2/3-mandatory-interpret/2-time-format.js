41ef6029c51da51bf40d07e7fd6e73c3d486997d
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-17 08:49:24 +0100

    Add solution comments for variable declarations in 2-time-format.js

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -23,3 +23,14 @@ console.log(result);
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+
+
+
+//Solution
+// a) There are 6 declared variables in this program, all declared with 'const'
+//   - const movieLength
+//   - const remainingSeconds
+//   - const totalMinutes
+//   - const remainingMinutes
+//   - const totalHours
+//   - const result
