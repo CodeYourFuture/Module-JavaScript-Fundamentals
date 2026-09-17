db7e9d1256cd529f583b341315aa8fb5b5d77ce7
Frumentius-Rev <tesfaybh@gmail.com>
2026-09-17 18:51:26 +0100

    complete count and random exercise

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -1,9 +1,9 @@
 let count = 0;
-console.log(count);
 
 count = count + 1;
 
-console.log(count);
-
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// Line 3: Adds 1 to count and assigns the result back to count.
+// The = operator assigns the new value to count.
