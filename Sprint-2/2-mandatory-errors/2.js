4a0c07f8f953926282b013f71c5219c75d8791fe
maryam-devio <maryamjanjua65@gmail.com>
2026-09-17 11:38:06 +0100

    Complete Sprint 2 error exercises

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,5 +1,10 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+// The error occurs because cityOfBirth is accessed before it has been initialized.
+// Variables declared with const cannot be accessed before their declaration is executed.
+// This causes a ReferenceError because the variable is in the Temporal Dead Zone (TDZ).
+// Moving the declaration before console.log fixes the error because cityOfBirth has a value before it is used.
