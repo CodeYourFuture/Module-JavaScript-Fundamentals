89a16d1c4f589012394e36d6b24d66ff0492435e
abmhts <abmhts@gmail.com>
2026-09-17 22:05:28 +0100

    Update-Explain random number generation with Math.random and Math.floor

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -7,10 +7,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 //num represents a randomly generated whole number between 1 and 100.
 
 // Try breaking down the expression and using documentation to explain what it means
-   // Math.random() generates a random decimal number greater than or equal to 0 and less than 1. Multiplying it by 100 gives a number between 0 and 100.
+   // Math.random() generates a random decimal number greater than or equal to 0 and less than 1. Multiplying by 100 gives a number between 0 and up to, but not including 100.
    // Math.floor() always rounds down and returns the largest integer less than or equal to a given number in this case between 0 and 99. Finally, adding 1 changes the range to 1–100.
    // Math.floor(Math.random() * (maximum - minimum + 1)) 
-   // Math.floor(Math.random() * (100 - 1 + 1)
+   // Math.floor(Math.random() * (100 - 1 + 1))
    // Math.floor(Math.random() * 100)
    // Math.random give any random number between 0 to 1 example 0.225 * 100 = 22.5
    // Math.floor rounds that number to a whole number so 22.5 will become 22
