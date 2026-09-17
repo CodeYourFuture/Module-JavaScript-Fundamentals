cc7acf910b432dad37013f2e5c6d3e23708e7e08
abmhts <abmhts@gmail.com>
2026-09-17 22:24:28 +0100

    update- Rename clock time variables starting with numbers

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,5 +1,6 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+//const 12HourClockTime = "8:53pm";
+//const 24hourClockTime = "20:53";
 
 const ClockTime12Hour = "8:53pm";
 const ClockTime24Hour = "20:53";
+console.log(ClockTime24Hour,ClockTime12Hour);
