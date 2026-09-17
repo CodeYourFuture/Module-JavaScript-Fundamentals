e44ee665b1b6ca29e0d715d99442fdb40f4ef785
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-17 12:36:12 +0100

    percentage-change.js task completed

--- Sprint-2/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-2/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -13,10 +13,19 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+// Answer: Line 4 'Number(), replaceAll()', line 5 'Number(), replaceAll()' and line 8, 100
+
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+// Answer: The error is coming from line 5, spotting at the Number(priceAfterOneYear.replaceAll("," ""));
+// // It's a synthax error missing a comma 
 
 // c) Identify all the lines that are variable reassignment statements
 
+// Answer: Line 4 and 5 are variable assignment statements
+
 // d) Identify all the lines that are variable declarations
 
+//  Answer: Line 1,2,7 and 8 are variable declearations
+
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// Answer: ReplaceAll() function clears out all the commas in the strings, the number() converts the strings to numbers to help with calculation
