5bc72b38502a96c0cc2d2598ce5524c42e0de223
shirinpanahian <sh.panahian@gmail.com>
2026-09-17 14:08:53 +0100

    change variable position

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+// This error (Cannot access 'cityOfBirth' before initialization) is because we need to declare the variable before using it.
+
