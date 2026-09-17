b1dcb1c5b96dab331e81e3b7515211888b6a1338
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-17 11:19:25 +0100

    4-random task completed

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -2,8 +2,11 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+console.log(num)
 
-// In this exercise, you will need to work out what num represents?
-// Try breaking down the expression and using documentation to explain what it means
-// It will help to think about the order in which expressions are evaluated
-// Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// num variable that carries the result of the expressions evaluated.
+// (maximum - minimum) does basic math: 100 - 1 + 1 = 100
+// Math.floor() takes out all decimal and make them an integer.
+// Math.random() gives random numbers between 0 and 100.
+// + minimum add 1 at the end but randomise to the value between 0 and 100
