d7e51e92011dbcec4996eec32e86b552e76a225b
Roger Lee <lee.roger.fn@gmail.com>
2026-09-17 09:34:54 +0100

    answered Key exercise question 1 (Count).

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -4,3 +4,6 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//= mean assignment operator. Computer will calculate the result of "count + 1" and update the variable with the result.
+//In this question, variable "count" changed from 0 to 1.
