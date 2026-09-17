4e607b24aab6c98b6a2fc371b2badd301ad7133c
shirinpanahian <sh.panahian@gmail.com>
2026-09-17 23:43:32 +0100

    understand and answer question

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,19 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// 2. const penceStringWithoutTrailingP = penceString.substring(0,penceString.length - 1);
+//     create a new variable and put the string without p 
+
+// 3.  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+//     padStart() adds characters to the beginning of a string to make it 3 characters long. 
+//     because we need to convert it to pound and pence 
+
+// 4. const pounds = paddedPenceNumberString.substring(0,paddedPenceNumberString.length - 2);
+//    We remove the last 2 characters and store the remaining part of the string as pounds.
+
+// 5. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+ //     Gets the last two characters as pence.”
+
+ // 6. console.log(`£${pounds}.${pence}`);
+ //.   Finally combine pound and pence
