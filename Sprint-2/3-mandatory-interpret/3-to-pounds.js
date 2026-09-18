ea7dbc1f3c4af7d87e0877fa294b72ebbc4aa1e0
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-18 15:47:58 +0100

    Add detailed explanation for penceStringWithoutTrailingP processing in 3-to-pounds.js

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,9 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// 2. const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
+//    substring(0, length - 1): is a method that is invoked in the paragraph above to take every character in the string provided except the last one, leaving out the 'p'
+//    That is -> '399p' becomes '399'
+//    const penceStringWithoutTrailingP: used to store the value from 'penceString.substring(...)'
+  
