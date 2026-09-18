4fb372d6d1ff3e349159f8168e5ee83c23a3f634
Maharit Belai <maharit2912@gmail.com>
2026-09-18 21:39:01 +0100

    delet all comment lines

--- Sprint-2/1-key-exercises/3-paths.js
+++ Sprint-2/1-key-exercises/3-paths.js
@@ -1,25 +1,11 @@
-// The diagram below shows the different names for parts of a file path on a Unix operating system
-
-// ┌─────────────────────┬────────────┐
-// │          dir        │    base    │
-// ├──────┬              ├──────┬─────┤
-// │ root │              │ name │ ext │
-// "  /    home/user/dir / file  .txt "
-// └──────┴──────────────┴──────┴─────┘
-
-// (All spaces in the "" line should be ignored. They are purely for formatting.)
-
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
-
-
 const startDirIndex = filePath.lastIndexOf("Users");
-const dir = filePath.slice(startDirIndex , lastSlashIndex);
+const dir = filePath.slice(startDirIndex, lastSlashIndex);
 console.log(`The dir part of ${filePath} is ${dir}`);
 const dotIndex = filePath.lastIndexOf(".");
 const ext = filePath.slice(dotIndex);
 console.log(`The ext part of the ${filePath} is ${ext}`);
-
