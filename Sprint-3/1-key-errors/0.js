49088faf2ec4bd763de924feb1fb5df5096e680c
HM-127BTY <mills.hugh@yahoo.com>
2026-09-18 13:45:04 +0100

    Answered 0.js and added new code

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -4,10 +4,15 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
 
 // =============> write your explanation here
+// The str has already been called so when its been set to the "let = str" it wont work as its already part of the function, so the name needs to change.
 // =============> write your new code here
+function capitalise(str) {
+  let capitaliseStr = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
