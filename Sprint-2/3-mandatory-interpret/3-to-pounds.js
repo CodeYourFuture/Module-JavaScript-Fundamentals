68ca43b1ad61f8c34673f2e6af14a29be178cf37
Frumentius-Rev <tesfaybh@gmail.com>
2026-09-18 18:41:12 +0100

    Implement pence to pounds conversion

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,23 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+
+// 1. const penceString = "399p"
+// Initialises a string variable with the value "399p"
+
+// 2. penceString.substring(0, penceString.length - 1)
+// Removes the final "p", leaving "399"
+
+// 3. penceStringWithoutTrailingP.padStart(3, "0")
+// Makes the string 3 characters long by adding "0" at the beginning if needed.
+
+// 4. paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2)
+// Takes everything except the final two characters to get the pounds
+
+// 5. paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0")
+// Takes the final two characters to get the pence and adds "0" at the end if needed
+
+// 6. console.log(`£${pounds}.${pence}`)
+// Displays the price in pounds and pence, such as £3.99
+
