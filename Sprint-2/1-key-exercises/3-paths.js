f3bef1e68e6a5ea7a1f2180f71ac65768c4f0488
Maharit Belai <maharit2912@gmail.com>
2026-09-18 21:24:19 +0100

    declared a variable startDirIndex to save the start index of the string User

--- Sprint-2/1-key-exercises/3-paths.js
+++ Sprint-2/1-key-exercises/3-paths.js
@@ -16,7 +16,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 
 gi
 // Create a variable to store the ext part of the variable
-
+const startDirIndex = filePath.lastIndexOf("Users"):
 const dir = filePath.slice(filePath.lastIndexOf("Users"),lastSlashIndex);
 console.log(`The dir part of ${filePath} is ${dir}`);
 const dotIndex = filePath.lastIndexOf(".");
