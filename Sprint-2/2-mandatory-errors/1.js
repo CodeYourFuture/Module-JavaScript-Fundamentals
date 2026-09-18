dc9718339963d6a0932c43f0143b837949223c2b
Rahana Suleiman <rahanasuleiman8@gmail.com>
2026-09-18 19:13:32 +0100

    change age variable declaration from 'const' to 'let' to allow reassignment and explained the error it causes when attempted

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -1,4 +1,11 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+// The problem here is that 'const' creates a constant variable which cannot be reassigned, attempting to reassign (age = age + 1) throws a TypeError: Assignment to constant variable.
+
+
+//change 'const' to 'let' so the value can be updated
+// const age = 33;
+let age = 33;
 age = age + 1;
+
+// console.log(age);
