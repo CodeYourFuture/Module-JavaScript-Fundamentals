5b4ed9d3fc15bdbfa7a806c91a43a60a87aa6000
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-18 12:21:19 +0100

    task 3.js explained and completed

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -5,5 +5,4 @@ console.log(last4Digits)
 // The variable cardNumber is not a string and the slice() function works with strings or array
 
 // Consider: Why does it give this error? 
-// It gave that error brcause the slice() tool are not applicable to numbers.     
-// I wasn't sure what error to expect because I'm just starting to learn the slice() tool
+// It gave that error "TypeError: cardNumber.slice is not a function" brcause the slice() function tool are not applicable to numbers.     
