f0db277b6ce51502a93a1e6ef80ec224eeb81753
Qhama Gwele <lwandogwele52@gmail.com>
2026-09-18 13:03:48 +0200

    Complete 1-key-exwecises in Sprint-2

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// Line 3 takes the current value of count (0), adds 1 to it (0 + 1 = 1), and uses the assignment operator (=) to reassign and store this new value (1) back into the count variable.
--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -3,7 +3,13 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
-// In this exercise, you will need to work out what num represents?
-// Try breaking down the expression and using documentation to explain what it means
-// It will help to think about the order in which expressions are evaluated
-// Try logging the value of num and running the program several times to build an idea of what the program is doing
+console.log(num);
+// what does num represent?
+// num represents a random whole number (integer) between minimum (1) and maximum (100) inclusive.
+
+//Breakdown of the expression:
+// 1. Math.random() generates a decimal number from 0 (inclusive) up to, but not including, 1.
+// 2. (maximum - minimum + 1) calculates the range size (100 - 1 + 1 = 100).
+// 3. Math.random() * 100 scales the random decimal to a range between 0 and 99.999...
+// 4. Math.floor() rounds that value down to the nearest whole integer (0 to 99).
+// 5. + minimum (+ 1) shifts the final value up into the target range of 1 to 100. 
