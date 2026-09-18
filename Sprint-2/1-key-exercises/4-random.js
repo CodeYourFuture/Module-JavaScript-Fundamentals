0bd5e8c08be7bb7459664eca9764bec1cb7c963e
Maharit Belai <maharit2912@gmail.com>
2026-09-18 22:15:14 +0100

    explain the expression in order of mathematical operation

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -3,7 +3,10 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
-// In this exercise, you will need to work out what num represents?
-// Try breaking down the expression and using documentation to explain what it means
-// It will help to think about the order in which expressions are evaluated
+//num represents a number
+//lets start by (maximum-minimum +1) which the output is 100. 
+//math.random()*100 returns random number between 0 and 100
+//math.floor()it rounds a number to thier nearest integer,
+//at the end we add 1.
+
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
