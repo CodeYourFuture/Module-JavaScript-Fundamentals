ba171acf5985ce3f382c910cd461ec74d183bb3d
Qhama Gwele <lwandogwele52@gmail.com>
2026-09-18 13:36:52 +0200

    Fix all bugs in 2-mandatory-errors

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,5 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
+// Answer: cityOfBirth was referenced before it was declared (ReferenceError).
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
