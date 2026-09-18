fdcbeb0a1f51d147c00c81565aaf32462cfad579
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-18 17:22:22 +0100

    Clarify explanation for paddedPenceNumberString processing in 3-to-pounds.js

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -34,7 +34,7 @@ console.log(`£${pounds}.${pence}`);
 // 3. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
 //    penceStringWithoutTrailingP is now '399'
 //    padStart(3, "0") is a method used above which guarantees that the numeric string is at least 3 characters long, and if it is shorter add '0' to the front until it is 3 characters long.
-//    padding to 3 ensures there is always at least one pounds digit and two pence digits. 
+//    padding to 3 ensures there is always at least 3 digits, that is, one pounds digit and two pence digits. 
 //    
 
 // 4. const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
