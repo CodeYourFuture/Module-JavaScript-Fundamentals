c41aa67de179641572efb5c08090654cf1bb80c2
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-18 12:46:01 +0100

    task 4.js completed and explained

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,3 +1,6 @@
 const HourClockTime1 = "8:53pm";
 const hourClockTime2 = "20:53";
 console.log(`${HourClockTime1}, ${hourClockTime2}`)
+
+//  The code displayed the error "SyntaxError: Invalid or unexpected token", this is because javaScript does not understand the variable name format.
+// This error is solved by renaming the variable name starting it with an alphabet and avoiding starting the variable with a number.
