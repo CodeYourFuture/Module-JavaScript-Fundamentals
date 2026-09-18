4d06615dfc751482f71fd3e77dc766b4fdda1a20
Maharit Belai <maharit2912@gmail.com>
2026-09-18 22:29:22 +0100

    format document

--- Sprint-2/1-key-exercises/2-initials.js
+++ Sprint-2/1-key-exercises/2-initials.js
@@ -2,11 +2,5 @@ const firstName = "Creola";
 const middleName = "Katherine";
 const lastName = "Johnson";
 
-
-
- 
-
 const initials = `${firstName[0]}${middleName[0]}${lastName[0]}`;
 console.log(initials);
-
-
--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -4,9 +4,8 @@ const maximum = 100;
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 //num represents a number
-//lets start by (maximum-minimum +1) which the output is 100. 
+//lets start by (maximum-minimum +1) which the output is 100.
 //math.random()*100 returns random number between 0 and 100
 //math.floor()it rounds a number to thier nearest integer,
 //at the end we add 1.
-console.log(num)
-
+console.log(num);
