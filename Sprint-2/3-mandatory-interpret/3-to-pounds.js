2baa16af5e983798a304c8bc799bcc283a72fc5d
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-18 17:18:13 +0100

    Add detailed explanation for pence calculation in 3-to-pounds.js

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -35,9 +35,17 @@ console.log(`£${pounds}.${pence}`);
 //    penceStringWithoutTrailingP is now '399'
 //    padStart(3, "0") is a method used above which guarantees that the numeric string is at least 3 characters long, and if it is shorter add '0' to the front until it is 3 characters long.
 //    padding to 3 ensures there is always at least one pounds digit and two pence digits. 
+//    
 
 // 4. const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
 //    since paddedPenceNumberString is '399' and it's '.length' is 3
 //    this line is saying: '399.substring(0, 3 - 2)', which becomes '399.substring(0, 1)'
 //    'substring' is a method used to grab characters from a string, starting from the left, from position '0' up to, but not including, position '1'
 //    'const pounds' would be assigned the new value: '3'
+
+// 5. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+//    since paddedPenceNumberString.length is '3'
+//    then 'paddedPenceNumberString.substring(3 - 2)' becomes 'paddedPenceNumberString.substring(1)': this means grab everything from position '1' of the string to the end.
+//    That is -> '399.substring(1)' becomes '99' 
+//    '.padEnd(2, "0") is another method that ensures the string is at least 2 characters long and adding '0' to the end if it's too short.
+//    'const pence' would be assigned the new value: '99'
