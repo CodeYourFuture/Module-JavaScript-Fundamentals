08af9b8603e2381660b884af2a49bdeffaa50bad
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-18 12:53:26 +0100

    completed and printed task 1-count.js

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -1,5 +1,6 @@
 let count = 0;
 
 count = count + 1;
+console.log(count)
 
 // Line 3 has been able to reassign the value of the count variable from 0 to 1. The operator = assign the new value to be stored as 1.
