3231aadb9162d227cd5721d3edaa0355caa42145
Maharit Belai <maharit2912@gmail.com>
2026-09-19 03:13:19 +0100

    describ line by line code

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -2,13 +2,13 @@ const penceString = "399p";
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
-  penceString.length - 1
+  penceString.length - 1,
 );
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
 const pounds = paddedPenceNumberString.substring(
   0,
-  paddedPenceNumberString.length - 2
+  paddedPenceNumberString.length - 2,
 );
 
 const pence = paddedPenceNumberString
@@ -25,3 +25,15 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+
+// 2. penceStringWithoutTrailingP : it's value is the return of the method substring()which is 399.
+// the substring(0,3-1),when the substring extracts string  from index 0 to 2 =>399 and p will be dropped.
+
+// 3. paddedPenceNumberString  : its value is what the padstart() method returns which is the same.399
+//padstart(3,0),it is supossed to add  0's till the needed length is reached which is 3=>399.
+
+// 4. const Pound is initialized by the out come of the substring()method which is only 3. Because the
+// substring start index is zero and end endex is 1.
+// 5. const pence will be initialized there are two methods. the first one will extract the string form index 1
+// the second one will add  0 which there is no need=> 99
+//6.consol.log => 3.99
