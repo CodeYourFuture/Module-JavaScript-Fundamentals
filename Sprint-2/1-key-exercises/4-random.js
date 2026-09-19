7ca09e8b440293058bd1f3c47385ef391de61ebd
Mahir Shah <mahir.shah@hotmail.com>
2026-09-19 15:31:56 +0100

    Explain num and expression

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -7,3 +7,12 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+//num is a random, rounded down number between 100 and 1.
+//math.floor rounds down.
+//math.random gives a random number between 0 and 1. It can be equal to 0, but not equal to 1,
+//(maximum - minimum + 1) gives in this case 100
+//So then Math.random() * (maximum - minimum + 1) gives a number between 0 and 99
+//Math.random() * (maximum - minimum + 1) gives a number between 1 and 100
+//and thus Math.floor(Math.random() * (maximum - minimum + 1)) + minimum; gives a whole rounded 
+//down number
