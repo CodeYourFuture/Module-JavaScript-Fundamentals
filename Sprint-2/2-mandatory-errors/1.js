ea56b8e8164227882a704ec49f5571f13c8e4d95
Mahir Shah <mahir.shah@hotmail.com>
2026-09-19 15:36:56 +0100

    Explain error and how to fix

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -2,3 +2,8 @@
 
 const age = 33;
 age = age + 1;
+
+//TypeError: Assignment to constant variable.
+//Age is constant so can't be reassigned 
+//To fix, change const to let, so line 3 becomes:
+//let age = 33;
