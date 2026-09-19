a56a0879efb2fef7bda9814db22c491251dba1a7
Mahir Shah <mahir.shah@hotmail.com>
2026-09-19 15:39:13 +0100

    Explain error and how to fix

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -3,3 +3,7 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+//ReferenceError: Cannot access 'cityOfBirth' before initialization
+//Trying to use cityOfBirth before it's defined
+//To fix, put line 5 before line 4, so it is initialised before use
