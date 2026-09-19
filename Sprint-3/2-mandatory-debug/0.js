ac68c4a1e0255c8da6c9d940ac9c12f58c21c04c
HM-127BTY <mills.hugh@yahoo.com>
2026-09-19 11:39:39 +0100

    2-mandatory-debug 0.js Answered questions and fixed code.

--- Sprint-3/2-mandatory-debug/0.js
+++ Sprint-3/2-mandatory-debug/0.js
@@ -1,14 +1,23 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// syntax error as console.log(a, b) cant be part of the function.
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// I was wrong, it is showing the answer with console.log but giving undefined for the other as the function is incomplete.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  let answer = (a * b);
+  return answer
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
