fe9563cb707c506a654c09077ade58df6845f34d
Carol <cnassuna@gmail.com>
2026-09-19 15:28:10 +0100

    updated the paths file with my solution

--- Sprint-2/1-key-exercises/3-paths.js
+++ Sprint-2/1-key-exercises/3-paths.js
@@ -13,11 +13,16 @@ const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/");
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
+// console.log('The dir path of ${dir} is ${filePath}.${lastSlashIndex}'); 
+
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex);
+const ext = base.slice(base.lastIndexOf("."));
+
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The ext part of ${base} is ${ext}`);
 
 // https://www.google.com/search?q=slice+mdn
