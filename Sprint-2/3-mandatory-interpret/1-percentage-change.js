c52de969692bd8072ca1325aea1da21b19e9847e
Maharit Belai <maharit2912@gmail.com>
2026-09-19 01:02:10 +0100

    delet the question in the comment and edit my answer

--- Sprint-2/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-2/3-mandatory-interpret/1-percentage-change.js
@@ -9,17 +9,11 @@ const percentageChange = (priceDifference / carPrice) * 100;
 
 console.log(`The percentage change is ${percentageChange}`);
 
-// Read the code and then answer the questions below
 
-// a) How many function calls are there in this file? Write down all the lines where a function call is made
-       // There are two function calls. line 4 and 5
-// b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-        //The error iscoming from the line 5, because there was no comma in the replaceAll method .
-// c) Identify all the lines that are variable reassignment statements
-         // line 4 and 5 are the variable reassignment statments.
 
-// d) Identify all the lines that are variable declarations
-          // line 1 ,2 ,7 and 8 are lines where variable declarions happened
 
-// e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
-         // the method .replaceAll is replacing the , in the 10,000 and throws it 10000, the number function will convert it into a number.
+        // There are two function calls. line 4 and 5
+        //The error is coming from the line 5, because there was no comma in the replaceAll() method .
+        // line 4 and 5 are the variable reassignment statments.
+        // line 1 ,2 ,7 and 8 are lines where variable declarions happened
+         // the method .replaceAll is replacing the coma (,) in the 10,000 and throws it 10000, the number function converts it into a number.
