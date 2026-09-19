4acb0269142e8bedde43526663679e23d8886056
Carol <cnassuna@gmail.com>
2026-09-19 16:17:43 +0100

    updated the random file with my understanding of the logic

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -2,8 +2,14 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+// (0.68 * 100 ) + 1
+console.log(num)
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// Answer: For calculations i utilised BODMAS formula solving numbers in brackets first, multiplication, subtraction and addition
+// I used 0.68 for math.floor(random number) + 1
+// Sum = 69
