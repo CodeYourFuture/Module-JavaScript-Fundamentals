23673eeb973fd675c17f53cbc8f9d3233e56f924
Rahana Suleiman <rahanasuleiman8@gmail.com>
2026-09-19 10:23:50 +0100

    Fix: update in 3-paths.js

--- Sprint-2/1-key-exercises/3-paths.js
+++ Sprint-2/1-key-exercises/3-paths.js
@@ -15,13 +15,13 @@ const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
-const dir = filePath.slice(0, lastSlashIndex + 1);
+const dir = filePath.slice(0, lastSlashIndex);
 
 // Create a variable to store the ext part of the variable
 const lastDotIndex = filePath.lastIndexOf(".");
-const ext = filePath.slice(lastDotIndex + 1);
+const ext = filePath.slice(lastDotIndex);
 
-// console.log(dir);
-// console.log(ext);
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
 
 // https://www.google.com/search?q=slice+mdn
