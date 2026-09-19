3c79ff2cf914f1f789b18dfc1fb3f3e45196af27
Maharit Belai <maharit2912@gmail.com>
2026-09-19 01:55:46 +0100

    run the code and edit my answers

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -1,4 +1,4 @@
-const movieLength = 8784; // length of movie in seconds
+const movieLength = 3000; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -9,17 +9,21 @@ const totalHours = (totalMinutes - remainingMinutes) / 60;
 const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
 console.log(result);
 
-// For the piece of code above, read the code and then answer the following questions
 
-// a) How many variable declarations are there in this program?
 
-// b) How many function calls are there?
 
-// c) Using documentation, explain what the expression movieLength % 60 represents
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+//A.There are six variable declararions
 
-// d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+//B. no function calls
 
-// e) What do you think the variable result represents? Can you think of a better name for this variable?
+// C.The % represents a remainder. The operator returns the remainder leftover when one operand  which in
+    //  this cas is movieLength is divided by a second operand in this case 60%.
 
+
+// D.it means it is changing the value that was in second into minute by dividing it 60 after the top calculation makes it whole number.
+
+//E. It represents the duration of the movie.movieLength/movieDuration.
+ 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+  
+// yes it works, i experimented it with various values. I found it to be working properly.
