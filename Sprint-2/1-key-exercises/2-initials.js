23d720338e29b6ec5d0f7b3a64e9f44ce468f8cd
Mahir Shah <mahir.shah@hotmail.com>
2026-09-19 14:01:09 +0100

    Change initial to const and define at initialisation

--- Sprint-2/1-key-exercises/2-initials.js
+++ Sprint-2/1-key-exercises/2-initials.js
@@ -5,9 +5,7 @@ const lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
-initials += firstName.charAt(0)
-initials += middleName.charAt(0)
-initials += lastName.charAt(0)
+const initials = `firstName.charAt(0) + middleName.charAt(0) + lastName.charAt(0)`;
+
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
