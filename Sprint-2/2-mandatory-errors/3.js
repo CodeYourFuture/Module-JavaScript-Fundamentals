3daf70a5429ff680adcce9778e89d5ce1dd75153
Maharit <maharit2912@gmail.com>
2026-09-20 19:43:54 +0100

    run prettier

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -1,6 +1,6 @@
 const cardNumber = "4533787178994213";
 const last4Digits = cardNumber.slice(-4);
 
-// the code is not working because the number initialized should be in "" . 
+// the code is not working because the number initialized should be in "" .
 // so it can know it/or treat it as a string.
 console.log(last4Digits);
