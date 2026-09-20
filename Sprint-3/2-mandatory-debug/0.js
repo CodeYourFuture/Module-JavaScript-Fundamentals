2b628024076898631a8c850ac1c6000641d2b296
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-09-20 14:48:35 +0100

    consoled log the function and further explained my prediction and the results

--- Sprint-3/2-mandatory-debug/0.js
+++ Sprint-3/2-mandatory-debug/0.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-// This function would multiply the two arguments a and b whatever that is
+// This function would multiply the two arguments a and b 
 // when we log the output we will get the string and the output of the function but because we are not returning an output we might run into an error 
 
 // function multiply(a, b) {
@@ -11,7 +11,8 @@
 // console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
-//The console.log() inside the funtion will print a*b but when we call the function will return undefined 
+//The console.log() inside the function will print a*b but when we call the function will return the out put and undefined 
+// but after the fix of returning instead it will be working giving the result in that instance will get 320
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
@@ -20,3 +21,5 @@ function multiply(a, b) {
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+console.log(multiply(10,32))
