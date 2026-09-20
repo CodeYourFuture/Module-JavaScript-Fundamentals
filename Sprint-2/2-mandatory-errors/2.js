b2957b1d4008d770f2d69c460eff760692f30c33
Frumentius-Rev <tesfaybh@gmail.com>
2026-09-20 19:11:55 +0100

    Fix variable initialization error

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,8 +1,10 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
 
-// Error: ReferenceError: Cannot access 'cityOfBirth' before initialization
-// Why: cityOfBirth is used before it is declared.
+
+// Error: ReferenceError: 
+// Why: Cannot access 'cityOfBirth' before initialization
+// Fix: Declare the const before using it
