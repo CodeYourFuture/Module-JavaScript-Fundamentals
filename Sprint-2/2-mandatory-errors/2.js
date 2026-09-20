bd744ffcb5a21daf02b20669c45d357d7b0f3de8
Maharit <maharit2912@gmail.com>
2026-09-20 19:44:28 +0100

    Run Prettier

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,7 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-
 const cityOfBirth = "Bolton";
 // The error is that the const declation and initialization came after console.log so no access for cityOfBirth.
 console.log(`I was born in ${cityOfBirth}`);
