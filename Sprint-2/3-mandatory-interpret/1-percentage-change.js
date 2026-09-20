821164240b9ea1da618500ea695298718a40071d
Maharit <maharit2912@gmail.com>
2026-09-20 22:04:31 +0100

    edit my Answer in the comment section

--- Sprint-2/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-2/3-mandatory-interpret/1-percentage-change.js
@@ -2,18 +2,20 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," , ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
 
 console.log(`The percentage change is ${percentageChange}`);
 
-
-
-
-        // There are two function calls. line 4 and 5
-        //The error is coming from the line 5, because there was no comma in the replaceAll() method .
-        // line 4 and 5 are the variable reassignment statments.
-        // line 1 ,2 ,7 and 8 are lines where variable declarions happened
-         // the method .replaceAll is replacing the coma (,) in the 10,000 and throws it 10000, the number function converts it into a number.
+//1. There are 4 function calls in line 4 and 5
+//     * number()and the second one is .replaceall() in line 4
+//      * number() and the second one is .replaceall()in line 4
+// 2. The error is coming from the line 5, because there was no comma in the replaceAll() method .
+// 3. line 4 and 5 are the variable reassignment statments.
+// 4. line 1 ,2 ,7 and 8 are lines where variable declarions happened
+// 5. *The method .replaceAll() in line 4 is replacing the coma (,) with nothing("") in the "10,000" and the output is "10000",
+//     the number function converts the string "10000" into a number.
+//    *The method .replaceAll() in line 4 is replacing the coma(,) with nothing("") in the "8,543" and the output is "8543"
+//        the number function coverts the string "8543" into a number which is 8543.
