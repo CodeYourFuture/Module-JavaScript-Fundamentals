3ab1fb27bffd339d8e0438c1a7835f7fd780a880
Maharit <maharit2912@gmail.com>
2026-09-20 22:08:29 +0100

    change into original value of movieLength

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -1,4 +1,4 @@
-const movieLength = 3000; // length of movie in seconds
+const movieLength = 8784; // length of movie in seconds
 
 const remainingSeconds = movieLength % 60;
 const totalMinutes = (movieLength - remainingSeconds) / 60;
@@ -9,21 +9,17 @@ const totalHours = (totalMinutes - remainingMinutes) / 60;
 const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
 console.log(result);
 
-
-
-
 //A.There are six variable declararions
 
 //B. no function calls
 
-// C.The % represents a remainder. The operator returns the remainder leftover when one operand  which in
-    //  this cas is movieLength is divided by a second operand in this case 60%.
-
+//C.The % represents a remainder. The operator returns the remainder leftover when one operand  which in
+//  this cas is movieLength is divided by a second operand in this case 60%.
 
 // D.it means it is changing the value that was in second into minute by dividing it 60 after the top calculation makes it whole number.
 
 //E. It represents the duration of the movie.movieLength/movieDuration.
- 
+
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
-  
+
 // yes it works, i experimented it with various values. I found it to be working properly.
