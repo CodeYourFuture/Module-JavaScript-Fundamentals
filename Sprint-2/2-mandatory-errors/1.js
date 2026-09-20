1dd1e64146ce09a41eb0bb0e314ea62c2007701f
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-09-20 14:11:24 +0100

    interpreted the error of the 1.js file

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -2,3 +2,9 @@
 
 let age = 33;
 age = age + 1;
+
+/* 
+The error thrown was a TypeError indicating assignment to constant variable and this happen when 
+we declare a variable with const and try to give it a new value
+
+*/
