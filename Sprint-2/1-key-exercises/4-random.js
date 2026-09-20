2582f53913fa767760cc7283058e940aecf613b3
Maharit <maharit2912@gmail.com>
2026-09-20 20:54:08 +0100

    add comments to the comment section for further explanation

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -3,9 +3,11 @@ const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
-//num represents a number
-//lets start by (maximum-minimum +1) which the output is 100.
-//math.random()*100 returns random number between 0 and 100
-//math.floor()it rounds a number to thier nearest integer,
-//at the end we add 1.
+//1.num represents a number which is an Integer.
+//2.lets start by (maximum-minimum +1) which the output is 100.
+//3.math.random()*100 returns random number between 0 and 100, what actually
+//  happens is that math.randum gives out random number from 0 to 1 which 1 is 
+//  not included and in this case, multiply the out put by 100.
+//4.math.floor()it rounds a number to thier nearest integer, which means it drops all the decimal.
+//5.at the end we add 1 which is the minimum.
 console.log(num);
