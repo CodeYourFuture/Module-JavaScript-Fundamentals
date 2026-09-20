6b3c6ec3467631706a74f5ab1807adddb39b35eb
Maharit <maharit2912@gmail.com>
2026-09-20 19:39:00 +0100

    add console.log for const hourClockTime and add pm after 20:53

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,5 +1,6 @@
 const HourClockTime = "8:53pm";
-const hourClockTime = "20:53";
+const hourClockTime = "20:53pm";
 
 // The error here is that the variable started with number.
 console.log(HourClockTime);
+console.log(hourClockTime);
