caf0ecd86732d30b3db07866be1e5165fa0448dd
Frumentius-Rev <tesfaybh@gmail.com>
2026-09-20 18:45:22 +0100

    Fix age reassignment error

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -1,7 +1,9 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
+console.log(age);
 
 // Error: TypeError
-// Why: age was declared with const, so it cannot be reassigned.
+// Why: Assignment to constant variable.
+// Fix: Changed const to let, which allows the variable to be reassigned.
