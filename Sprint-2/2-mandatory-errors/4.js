c11551236a2445d1805985977d7213885c71f069
SalahAlsabhi <mahkoom87@gmail.com>
2026-09-20 11:19:30 +0100

    Fix invalid variable names starting with numbers in 4.js

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,2 +1,6 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const clockTime12Hour = "8:53pm";
+const clockTime24Hour = "20:53";
+console.log(clockTime12Hour);
+console.log(clockTime24Hour);
+
+//variable names can not start with numbers
