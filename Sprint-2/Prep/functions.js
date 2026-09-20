903b731a96ce427f317cfe97912eb6c0d93056dd
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-20 21:09:37 +0100

    lastIndexOf() function exercise

--- Sprint-2/Prep/functions.js
+++ Sprint-2/Prep/functions.js
@@ -1,47 +1,51 @@
-console.log("In this exercise I will be learning different functions, and preactising them. ")
+console.log("In this exercise I will be learning different functions, and preactising them. ");
 
-let userName = "Matthew"
-let location = "Manchester"
-let favoriteFood = "Jollof rice"
+let userName = "Matthew";
+let location = "Manchester";
+let favoriteFood = "Jollof rice";
 
 function myInfo(userName, location, favoriteFood) {
     console.log(`My name is ${userName}` )
     console.log(`I live in ${location}` )
     console.log(`My favorite food is ${favoriteFood}` )
-}
-myInfo(userName, location, favoriteFood)
+};
+myInfo(userName, location, favoriteFood);
 
-sliceUserName = userName.slice(0, 4)
-console.log(`My friends love to call me ${sliceUserName}`)
+sliceUserName = userName.slice(0, 4);
+console.log(`My friends love to call me ${sliceUserName}`);
 
-sliceLocation = location.lastIndexOf("h")
-console.log(sliceLocation)
+sliceLocation = location.lastIndexOf("h");
+console.log(sliceLocation);
 
-const number1 = "50"
-const number2 = "100"
-const result = Number(number1) + Number(number2)
-console.log(result)
+const number1 = "50";
+const number2 = "100";
+const result = Number(number1) + Number(number2);
+console.log(result);
 
-let hobby = "I love football and traveling"
-let moreHobby = hobby.replaceAll("football", "swiming").replaceAll("traveling", "hiking")
-console.log(moreHobby)
+let hobby = "I love football and traveling";
+let moreHobby = hobby.replaceAll("football", "swiming").replaceAll("traveling", "hiking");
+console.log(moreHobby);
 
-const passion = hobby.substring(6, 15)
-console.log(passion)
+const passion = hobby.substring(6, 15);
+console.log(passion);
 
-const luckyNumber = "7"
-const changeNumber = luckyNumber.padStart(5, "0")
-console.log(changeNumber)
+const luckyNumber = "7";
+const changeNumber = luckyNumber.padStart(5, "0");
+console.log(changeNumber);
 
-const anotherNumber = luckyNumber.padEnd(5, "0")
-console.log(anotherNumber)
+const anotherNumber = luckyNumber.padEnd(5, "0");
+console.log(anotherNumber);
 
-const carPrice = 30000.50
-const updatedCarPrice = Math.floor(carPrice)
-console.log(updatedCarPrice)
+const carPrice = 30000.50;
+const updatedCarPrice = Math.floor(carPrice);
+console.log(updatedCarPrice);
 
-const addNumber = 20
-const addNumber2 = 30
-const addNumber3 = addNumber + addNumber2
+const addNumber = 20;
+const addNumber2 = 30;
+const addNumber3 = addNumber + addNumber2;
 const addNumberResult = Math.random() * addNumber3
-console.log(addNumberResult)
+console.log(addNumberResult);
+
+const repeatedText = "one, two, three";
+const lastOnePosition = repeatedText.lastIndexOf("one");
+console.log(lastOnePosition);
