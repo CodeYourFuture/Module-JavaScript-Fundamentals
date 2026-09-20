f9203596e349d7b3963eaa1ec8906ea846ddc406
marscancode <marsadesina@gmail.com>
2026-09-20 20:10:17 +0100

    added prediction of why code on line 1 and 2 wont work

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -1,9 +1,13 @@
 const cardNumber = 4533787178994213;
 const last4Digits = cardNumber.slice(-4);
 
+
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Before running the code, make and explain a prediction about why the code won't work
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// I predict that it'll throw a syntax error as its not wrapped in "" so it's  registering as a number and but it needs to be a string so the .slice() method can register each individual number in last4Digits?  
+
