64eec019ab8380bc00727c3404f0b7614b5eff7b
SalahAlsabhi <mahkoom87@gmail.com>
2026-09-20 10:08:44 +0100

    Add explanation comment for 4-random.js

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -8,3 +8,4 @@ console.log(num);
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+// num is a random whole number between 1 and 100
