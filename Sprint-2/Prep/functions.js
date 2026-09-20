aedbcb2e0d7c79ebd4a483256f71161822a550b3
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-20 15:18:56 +0100

    created variables for personal information

--- Sprint-2/Prep/functions.js
+++ Sprint-2/Prep/functions.js
@@ -1,8 +1,8 @@
 console.log("In this exercise I will be learning different functions, and preactising them. ")
 
-// let userName = "Matthew"
-// let location = "Manchester"
-// let favoriteFood = "Jollof rice"
+let userName = "Matthew"
+let location = "Manchester"
+let favoriteFood = "Jollof rice"
 
 // function myInfo(userName, location, favoriteFood) {
 //     console.log(`My name is ${userName}` )
