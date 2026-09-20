005834efba1b427acc79af59a34eae3d4a34276b
Hanna Bohlin <ha.bohlin@gmail.com>
2026-09-20 23:32:48 +0100

    Deleted prep as those files don't belong in the PR

--- prep/parameters.js
+++
@@ -1,5 +0,0 @@
-function greet(timeOfDay, name = "user"){
-    console.log(`Good ${timeOfDay}, ${name}.`)
-}
-
-greet("afternoon");
--- prep/using-a-function.js
+++
@@ -1,4 +0,0 @@
-const rounded = Math.round(10.3);
-const rounded2 = Math.round(4.2);
-console.log(rounded, rounded2);
-console.lo
