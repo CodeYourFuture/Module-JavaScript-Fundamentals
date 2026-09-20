56bebf5602872afeaccd7d33340899f788fa7018
Maharit <maharit2912@gmail.com>
2026-09-20 19:14:01 +0100

    changethe declaration const to let

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -1,5 +1,6 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+let age = 33;
 age = age + 1;
 // you can not reassign a value to variable that is declared const. 
+console.log(age);
