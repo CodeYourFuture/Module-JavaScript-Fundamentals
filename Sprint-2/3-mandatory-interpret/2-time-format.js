ab458e3d4157dd4666673d969b5c7b2aef178a54
anitahy73 <anita.hy73@gmail.com>
2026-09-20 21:25:27 +0100

    Add answers to code interpretation questions
    Updated comments to provide answers to questions about the code.

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -11,15 +11,21 @@ console.log(result);
 
 // For the piece of code above, read the code and then answer the following questions
 
-// a) How many variable declarations are there in this program?
+// a) How many variable declarations are there in this program?6
 
-// b) How many function calls are there?
+// b) How many function calls are there?1
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
+//The % operator is called the remainder operator.
+//It gives you the remainder left over after division.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
 
-// d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+// d) Interpret line 4, what does the expression assigned to totalMinutes mean?This removes the seconds that don't make up a complete minute.
 
-// e) What do you think the variable result represents? Can you think of a better name for this variable?
+// e) What do you think the variable result represents? Can you think of a better name for this variable?creates a formatted representation of the movie's length in hours, minutes, and seconds.
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+	//Works properly for non-negative whole-number seconds; decimals/negative values cause formatting problems
+
+
+
