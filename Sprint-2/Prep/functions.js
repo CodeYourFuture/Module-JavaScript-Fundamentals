b5bda0554e76d137491c5760489782706172a17e
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-20 15:17:36 +0100

    print file purposes

---
+++ Sprint-2/Prep/functions.js
@@ -0,0 +1,16 @@
+console.log("In this exercise I will be learning different functions, and preactising them. ")
+
+// let userName = "Matthew"
+// let location = "Manchester"
+// let favoriteFood = "Jollof rice"
+
+// function myInfo(userName, location, favoriteFood) {
+//     console.log(`My name is ${userName}` )
+//     console.log(`I live in ${location}` )
+//     console.log(`My favorite food is ${favoriteFood}` )
+// }
+// myInfo(userName, location, favoriteFood)
+
+// sliceUserName = userName.slice(0, 4)
+// console.log(`My friends love to call me ${sliceUserName}`)
+
