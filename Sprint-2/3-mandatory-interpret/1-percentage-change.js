8c04eec5488241db3f4ba31dc90770cdab90ce4c
shirinpanahian <sh.panahian@gmail.com>
2026-09-20 19:19:44 +0100

    format documets

--- Sprint-2/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-2/3-mandatory-interpret/1-percentage-change.js
@@ -16,20 +16,19 @@ console.log(`The percentage change is ${percentageChange}`);
     replaceAll() - Line 4 and Line 5
     console.log() - Line 10*/
 
-
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-   /* line 5 , There is a comma missing between "," and ""*/
+/* line 5 , There is a comma missing between "," and ""*/
 
 // c) Identify all the lines that are variable reassignment statements
-    /* Line 4 :carPrice = Number(carPrice.replaceAll(",", ""));
+/* Line 4 :carPrice = Number(carPrice.replaceAll(",", ""));
        Line5 : priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
        carPrice and priceAfterOneYear were already declared on Line 1 and 2 using let */
 
 // d) Identify all the lines that are variable declarations
-       /* Line 1 : let carPrice = "10,000";
+/* Line 1 : let carPrice = "10,000";
           Line 2 : let priceAfterOneYear = "8,543";
           Line 7 : const priceDifference = carPrice - priceAfterOneYear;
           Line 8 : const percentageChange = (priceDifference / carPrice) * 100;*/
 
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
-        /*replaceAll remove all the commas from the string and Number convert string  to the number*/ 
+/*replaceAll remove all the commas from the string and Number convert string  to the number*/
