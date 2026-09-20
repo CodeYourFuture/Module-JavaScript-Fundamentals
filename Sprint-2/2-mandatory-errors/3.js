7ec69952493d7698d277519bf1f7f21aa506b83d
marscancode <marsadesina@gmail.com>
2026-09-20 20:28:06 +0100

    explained why my prediction was wrong

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -10,4 +10,6 @@ const last4Digits = cardNumber.slice(-4);
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 
 // I predict that it'll throw a syntax error as its not wrapped in "" so it's  registering as a number and but it needs to be a string so the .slice() method can register each individual number in last4Digits?  
-
+//console.log(last4Digits)
+//output: TypeError: cardNumber.slice is not a function
+//it's slightly different to what i expected as i thought it was solely about not using the right syntax but it's a type error which after research type error is  about not being able to perform a certain operation on a value so here the slice method wouldn't work as .slice() method can only work on strings and arrays so i need to turn cardNumber into a string before the slice() method can work on it. so whilst i was right that it needed "" my reason for why was wrong. 
