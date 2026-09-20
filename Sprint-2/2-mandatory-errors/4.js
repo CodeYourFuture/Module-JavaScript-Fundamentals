e63d651929cd34ddfafcd66f3113857d6e5c763c
Maharit <maharit2912@gmail.com>
2026-09-20 19:35:02 +0100

    add console.log for the const HourClockTime

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,4 +1,5 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const HourClockTime = "8:53pm";
+const hourClockTime = "20:53";
 
 // The error here is that the variable started with number.
+console.log(HourClockTime);
