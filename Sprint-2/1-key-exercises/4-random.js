9cc571debb149cc046c8805c9d695501412a34b5
Mahir Shah <mahir.shah@hotmail.com>
2026-09-21 16:12:15 +0100

    Correct explanation

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -8,11 +8,11 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
 
-//num is a random, rounded down number between 100 and 1.
+//num is a random whole number between 1 and 100 inclusive.
 //math.floor rounds down.
-//math.random gives a random number between 0 and 1. It can be equal to 0, but not equal to 1,
+//math.random gives a random number between 0 and 1. It can be equal to 0, but not equal to 1, and have decimals
 //(maximum - minimum + 1) gives in this case 100
-//So then Math.random() * (maximum - minimum + 1) gives a number between 0 and 99
-//Math.random() * (maximum - minimum + 1) gives a number between 1 and 100
+//So then Math.random() * (maximum - minimum + 1) gives a number between 0 inclusive and 100 exclusive
+//Math.random() * (maximum - minimum + 1) + minimum gives a number between 1 inclusive and 101 exclusive
 //and thus Math.floor(Math.random() * (maximum - minimum + 1)) + minimum; gives a whole rounded
 //down number
