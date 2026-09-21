4e3b86852b2c343398ff3126c6502eed126a0ae8
Maharit <maharit2912@gmail.com>
2026-09-21 23:56:14 +0100

    change  const result into movieDuration and edit comment

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -6,8 +6,8 @@ const totalMinutes = (movieLength - remainingSeconds) / 60;
 const remainingMinutes = totalMinutes % 60;
 const totalHours = (totalMinutes - remainingMinutes) / 60;
 
-const result = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
-console.log(result);
+const movieDuration = `${totalHours}:${remainingMinutes}:${remainingSeconds}`;
+console.log(movieDuration);
 
 //A.There are six variable declarations
 
@@ -19,11 +19,11 @@ console.log(result);
 // D.it means it is changing the value that was in second into minute by dividing it 60 after the top
 // calculation makes it whole number.
 
-//E. It represents the duration of the movie.movieLength/movieDuration.
+//E. It represents the duration of the movie.movieLength/ better name would be movieDuration.
 
 //F. No it doesnt work for all different values , I experimented it with -ve numbers and decimals.It doesnt
-//   give the desired output. For example if 
+//   give the desired output. For example if
 //                                      const movieLength = -60;
-//                                                 result = 0:-1:0, there is no -ve value in time
-//                                   if const movieLength = 90.5;                                  
-//                                                 result = 0:1:30.5 the out put is decimal second.
+//                                          movieDuration = 0:-1:0, there is no -ve value in time
+//                                   if const movieLength = 90.5;
+//                                          movieDuration = 0:1:30.5 the out put is decimal second.
