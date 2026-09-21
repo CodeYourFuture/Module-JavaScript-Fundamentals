7c792eef8ea8e53ce5eb620ca4973054448fa1e5
marscancode <marsadesina@gmail.com>
2026-09-21 17:41:39 +0100

    updated code on line 18

--- Sprint-2/1-key-exercises/3-paths.js
+++ Sprint-2/1-key-exercises/3-paths.js
@@ -12,10 +12,10 @@
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
 const lastSlashIndex = filePath.lastIndexOf("/"); //44
 const base = filePath.slice(lastSlashIndex + 1); //file.txt
-console.log(`The base part of ${filePath} is ${base}`);
+//console.log(`The base part of ${filePath} is ${base}`);
 
 const dir = filePath.slice(0, lastSlashIndex);
-const ext = base.slice(4);
+const ext = filePath.slice(-4);
 console.log(dir);
 console.log(ext);
 // https://www.google.com/search?q=slice+mdn
