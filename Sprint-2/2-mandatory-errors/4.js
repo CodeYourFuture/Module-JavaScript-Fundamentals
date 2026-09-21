aa75b0b1063b2b9e4ca6bbb966820655137d5a17
Maharit <maharit2912@gmail.com>
2026-09-21 22:57:33 +0100

    delet PM from line 2

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,5 +1,5 @@
 const HourClockTime = "8:53pm";
-const hourClockTime = "20:53pm";
+const hourClockTime = "20:53";
 
 // The error here is that the variable started with number.
 console.log(HourClockTime);
