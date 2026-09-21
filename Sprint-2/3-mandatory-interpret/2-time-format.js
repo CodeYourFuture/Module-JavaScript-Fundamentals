efa997dcfd8d47ffc0f701bdf0be8285b7f427d8
marscancode <marsadesina@gmail.com>
2026-09-21 10:39:30 +0100

    Identified 1 function call in file

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -15,6 +15,7 @@ console.log(result);
 //There are 6 variable declarations.
 
 // b) How many function calls are there?
+// There is 1 function call the console.log
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
