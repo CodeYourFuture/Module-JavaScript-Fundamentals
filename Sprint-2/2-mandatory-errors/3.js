e9cb419f6444e13c87fd3d7846ffa7a4a6a1d7b2
Selamawit Abay <Sulamatis3611@gmail.com>
2026-09-21 17:05:01 +0100

    answer 2.3 e

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -8,7 +8,7 @@ const last4Digits = cardNumber.slice(-4);
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
 
-Convert cardNumber to a string before using .slice():
-const cardNumber = 4533787178994213;
-const last4Digits = String(cardNumber).slice(-4);
-console.log(last4Digits);
+// Convert cardNumber to a string before using .slice():
+// const cardNumber = 4533787178994213;
+// const last4Digits = String(cardNumber).slice(-4);
+// console.log(last4Digits);
