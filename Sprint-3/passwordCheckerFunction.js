d10542b3569e34c520b68f33603bb9d6766a2a6c
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-21 15:53:32 +0100

    return exercise completed

--- Sprint-3/passwordCheckerFunction.js
+++ Sprint-3/passwordCheckerFunction.js
@@ -1,21 +1,71 @@
 // Already have the password stored in a variable;
-const password = "Mattcarolus10";
+// Exercise one: Using console.log and calling function
+// const password = "Mattcarolus10";
+
+// // Receive the value which the user entered;
+// function checkPassword(userInput) {
+
+// // Compare the two values;
+// if (userInput===password) {
+
+// // If they match print "Correct password entered";
+//     console.log("Correct password entered ")
+// }
+
+// else {
+// // If they don't match print "Incorrect password, please try again";
+//     console.log("Incorrect password, please try again")
+// };
+
+// };
+// checkPassword("Mattcarolus10");
+
+
+
+// // Exercise 2: Using the variable scope "response"
+// const password2 = "Mattcarolus10";
+
+// // Receive the value which the user entered;
+// function checkPassword(userInput) {
+
+// // Compare the two values;
+// if (userInput===password2) {
+
+// // If they match print "Correct password entered";
+//     response = "Correct password entered "
+// }
+
+// else {
+// // If they don't match print "Incorrect password, please try again";
+//     response = "Incorrect password, please try again"
+// };
+
+// };
+// checkPassword("Mattcarolus");
+// console.log(response)
+
+
+
+const password3 = "Mattcarolus10";
+const output = checkPassword("Mattcarolus10");
 
 // Receive the value which the user entered;
 function checkPassword(userInput) {
+    let response;
 
 // Compare the two values;
-if (userInput===password) {
+if (userInput===password3) {
 
 // If they match print "Correct password entered";
-    response = "Correct password entered "
+    response = "Correct password entered ";
 }
 
 else {
 // If they don't match print "Incorrect password, please try again";
-    response = "Incorrect password, please try again"
-};
+    response = "Incorrect password, please try again";
+}
+return response;
+}
+console.log(output);
+ 
 
-};
-checkPassword("Mattcarolus");
-console.log(response)
