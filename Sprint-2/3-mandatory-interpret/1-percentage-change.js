2ad51f89c7ca8253dce90f4fd3f57548d182f03e
Carol <cnassuna@gmail.com>
2026-09-21 20:04:05 +0100

    renamed branch and pushed files to a previous branch long story

--- Sprint-2/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-2/3-mandatory-interpret/1-percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -12,11 +12,19 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
-
+// 2
+//carPrice = Number(carPrice.replaceAll(",", ""));
+//priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ","));
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-
+//b) error = SyntaxError: missing ) after argument list
+// (",", ",")); - added , between quoted values
 // c) Identify all the lines that are variable reassignment statements
-
+//carPrice = Number(carPrice.replaceAll(",", ""));
+//priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ","));
 // d) Identify all the lines that are variable declarations
-
+//let carPrice = "10,000";
+//let priceAfterOneYear = "8,543";
+//const priceDifference = carPrice - priceAfterOneYear;
+//const percentageChange = (priceDifference / carPrice) * 100;
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+// e) cleans the amount format by removing characters such as , and leaving only number
--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -1,19 +1,13 @@
 const penceString = "399p";
 
 const penceStringWithoutTrailingP = penceString.substring(
-  0,
-  penceString.length - 1
-);
+  0, penceString.length - 1);
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-const pounds = paddedPenceNumberString.substring(
-  0,
-  paddedPenceNumberString.length - 2
-);
+const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
 
-const pence = paddedPenceNumberString
-  .substring(paddedPenceNumberString.length - 2)
-  .padEnd(2, "0");
+
+const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
 
 console.log(`£${pounds}.${pence}`);
 
@@ -25,3 +19,8 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2. const penceStringWithoutTrailingP = penceString.substring(0): sets penceStringWithoutTRailingP = 399.0 - penceString -1 = 39
+// 3. const paddedPenceNumberString - ensures the figure is 3 characters to taking us back to 399
+// 4 const pounds - removes 2 characters from the amount = 3
+// 5. const pence - adds the amount by 2 characters taking us back to either 39 or 99
+// 6. console displays the figures in pounds and pence = 3.99
