63b65ec0804ca13125e8ca6925280e182dfb405b
Maharit <maharit2912@gmail.com>
2026-09-21 22:21:53 +0100

    add comment to the explanation formed on line 4.

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -8,6 +8,8 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 //3.math.random()*100 returns random number between 0 and 100, what actually
 //  happens is that math.randum gives out random number from 0 to 1 which 1 is
 //  not included and in this case, multiply the output by 100.
-//4.math.floor()it rounds a number to thier nearest integer, which means it drops all the decimal.
+//4.math.floor() gives out the largest intiger which is less than or equal to
+//  the given number,which is decimal, which means it drops all the decimal. forexample if 
+// the output is 69.5, it compares 69 with 69.5.The output will be 69 which is the largest Integer.
 //5.at the end we add 1 which is the minimum.
 console.log(num);
