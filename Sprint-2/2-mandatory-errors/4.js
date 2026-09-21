5655f66e52723e9c6d0528103879d2730fa2ae19
Sakiya Mayow <zakia.logic@gmail.com>
2026-09-21 03:38:24 +0100

    variables unable to start with a number, caused syntaxerror

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,2 +1,12 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const Hour12ClockTime = "8:53pm";
+
+console.log(Hour12ClockTime);
+
+const hour24ClockTime = "20:53";
+
+console.log(hour24ClockTime);
+
+
+
+
+Variable names cannot start with a number as Javascript doesnt allow it, it can contan number inside the variable but not at the beginning.
