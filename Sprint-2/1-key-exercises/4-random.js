8a38be1f5d7eff076481ed56554668ad17d489aa
anitahy73 <anita.hy73@gmail.com>
2026-09-21 23:04:28 +0100

    Clarify comments on random number generation
    Updated comments to clarify the random number generation process.

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -4,7 +4,7 @@ const maximum = 100;
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
 // In this exercise, you will need to work out what num represents?It gives us a random number between 1-100
-// Try breaking down the expression and using documentation to explain what it means
+// Try breaking down the expression and using documentation to explain what it means? + minimum shifts it between 1-100 so the minimum number is 1 and the biggest one is 100
  //Math.random gives us a random number between 0-1 and the next parantecec returns 100
 //after that we multiple these two given values between 0-100 and the floor methode rounds down the given value
 // It will help to think about the order in which expressions are evaluated
