ff405153cad85693cb840102bb5a2e05f9643244
Maharit <maharit2912@gmail.com>
2026-09-21 23:17:04 +0100

    changes made to the declared variables name

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,6 +1,6 @@
-const HourClockTime = "8:53pm";
-const hourClockTime = "20:53";
+const am_pm = "8:53pm";
+const militaryTime = "20:53";
 
 // The error here is that the variable started with number.
-console.log(HourClockTime);
-console.log(hourClockTime);
+console.log(am_pm);
+console.log(militaryTime);
