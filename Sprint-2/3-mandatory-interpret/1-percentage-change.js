34357e2434c011f17b271a0a9f71fcc050ffbc8d
marscancode <marsadesina@gmail.com>
2026-09-21 10:06:29 +0100

    Identified variable reassignment statements

--- Sprint-2/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-2/3-mandatory-interpret/1-percentage-change.js
@@ -17,6 +17,7 @@ console.log(`The percentage change is ${percentageChange}`);
 //  The error message in the terminal tells me there is a syntax error on line 5. There is a comma missing separating the two arguments. I can fix the problem by adding a comma. The console now prints "The percentage change is 14.57".
 
 // c) Identify all the lines that are variable reassignment statements
+// There are 2 variable reassignment statements in this file carPrice and priceAfterOneYear as both were first declared on line 1 and 2 using the let keyword before being reassigned on lines 4 and 5.
 
 // d) Identify all the lines that are variable declarations
 
