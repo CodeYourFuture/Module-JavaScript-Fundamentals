a83fbd0f71fb90413dd138b1fb0ad42b1380c18b
Roger Lee <lee.roger.fn@gmail.com>
2026-09-21 14:37:29 +0100

    keep only one error line.

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -5,17 +5,4 @@ const cityOfBirth = "Bolton";
 console.log(`I was born in ${cityOfBirth}`);
 
 //Initiation should access before the declaration.
- //error message: Error: ReferenceError: Cannot access 'cityOfBirth' before initialization
- //   at Module._resolveFilename (node:internal/modules/cjs/loader:1564:15)
- //   at wrapResolveFilename (node:internal/modules/cjs/loader:1118:27)
- //   at defaultResolveImplForCJSLoading (node:internal/modules/cjs/loader:1142:10)
- //   at resolveForCJSWithHooks (node:internal/modules/cjs/loader:1169:12)
- //   at Module._load (node:internal/modules/cjs/loader:1341:5)
- //   at wrapModuleLoad (node:internal/modules/cjs/loader:261:19)
- //   at Module.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:154:5)
- //   at node:internal/main/run_main_module:33:47 {
- // code: 'MODULE_NOT_FOUND',
- // requireStack: []
-
-
-// Node.js v24.21.0
+//error message: Error: ReferenceError: Cannot access 'cityOfBirth' before initialization
