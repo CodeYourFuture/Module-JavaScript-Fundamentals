e9d4ba359b183d463ca7e4c9a6a8b024689ff79f
russom <russom.g.meskel@gmail.cpm>
2026-09-21 21:11:25 +0100

    Update Math.random method description

--- Sprint-2/1-key-exercises/4-random.js
+++ Sprint-2/1-key-exercises/4-random.js
@@ -9,7 +9,7 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
 
 // 1. num is a variable that stores the final result of the expression assigned to it.
-// 2. The Math.random() method generates a random decimal number from 0 up but not excluding 1.
+// 2. The Math.random() method returns a pseudo-random floating number between 0(inclusive) and 1(exclusive).
 // 3. The Math.floor() method round a decimal number down to a whole number.
 // 4. (maximum - minimum + 1) calculates how many possible whole numbers there are. 
 //    Java Script evaluates the expressions inside the inner parentheses first. 
