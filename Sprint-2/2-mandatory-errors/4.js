a12ff6e58df7a4b0e18d05ec7fc42b89054944da
Roger Lee <lee.roger.fn@gmail.com>
2026-09-21 09:35:39 +0100

    modified the answer of 2-mandatory-errors 4.js

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,4 +1,5 @@
-const twelveHourClockTime = "8:53pm";
+const twelveClockTime = "8:53pm";
 const twentyFourHourClockTime = "20:53";
 
 //integer and number could not start for the initiation.
+//SyntaxError: Invalid or unexpected token - print before modified the initiation.
