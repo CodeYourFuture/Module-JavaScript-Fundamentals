744d864d3450d287b916c21ccfe8e822f5a7f87f
Mahir Shah <mahir.shah@hotmail.com>
2026-09-21 16:16:49 +0100

    Fix variable name by moving number to end of name. Fix formatting

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,6 +1,6 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+const HourClockTime12 = "8:53pm";
+const hourClockTime24 = "20:53";
 
 //SyntaxError: Invalid or unexpected token
 //In javascript, variable names cannot start with a number.
-//To fix, either put number at the end or start with _
+//To fix, either put number at the end or start with _ or use a different name
