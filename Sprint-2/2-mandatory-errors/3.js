9977d3253fa7f7392226240aa730a029993f6bec
Diana <40689028+ausiejute@users.noreply.github.com>
2026-09-22 00:56:28 +0100

    Completed all tasks in mandatory errors folder

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -1,5 +1,7 @@
-const cardNumber = 4533787178994213;
+const cardNumber = "4533787178994213";
+//const last4Digits = cardNumber.slice(-4);
 const last4Digits = cardNumber.slice(-4);
+console.log(last4Digits);
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +9,9 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+// I thought it was something to do with the slice "-4" (but i turned out i forgot that negative indices start at -1, not 0)
+// The actual error indicates that on the 3rd line there is a typeError: "cardNumber.slice is not a function"
+// Checked the error reference and decided to look more closely. Noticed that the card number is used as a number,
+// so it answers why the function couldn't be called - because they can be only called on Arrays and Strings.
+// Therefore, I'll add parentheses to turn the card number into a string (so that the function could be called)
--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,2 +1,9 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
+// const 12HourClockTime = "8:53pm";
+// const 24hourClockTime = "20:53";
+
+// SyntaxError: Invalid or unexpected token.
+// it turns out that in JS variable names cannot begin with a number
+
+const HourClockTime12 = "8:53pm";
+const HourClockTime24 = "20:53";
+console.log(HourClockTime24);
