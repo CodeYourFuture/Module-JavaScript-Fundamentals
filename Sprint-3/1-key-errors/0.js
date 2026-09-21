5433734ab6011d4308b21996f406730926dcd39a
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-21 11:50:41 +0100

    Refactor capitalise function to avoid SyntaxError by renaming variable

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -7,13 +7,18 @@
 // interpret the error message and figure out why an error is occurring
 // Error message interpreted ====> The error message simply means the name 'str' was declared twice in the same scope
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
-capitalise("moses");
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
+// capitalise("moses");
 // =============> write your explanation here
 // The error here is because the parameter 'str' and the variable 'let str' share the same name in he same scope, thus the SyntaxError.
 // To fix this error, I could either give the variable a different name or reassign the parameter 'str' without 'let', since no new declaration is made
 
 // =============> write your new code here
+function capitalise(str) {
+  let capitalised = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return capitalised;
+}
+console.log(capitalise("moses"));
