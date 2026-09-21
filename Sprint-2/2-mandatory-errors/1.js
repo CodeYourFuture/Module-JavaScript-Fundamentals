9a8d497dee71f1752d4611fed0970fff25b1d466
marscancode <marsadesina@gmail.com>
2026-09-21 17:28:20 +0100

    updated file with error type and explanation

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -4,3 +4,6 @@ let age = 33;
 age = age + 1;
 
 console.log(age);
+
+// TypeError: Assignment to constant variable.
+//This means javascript cannot reassign const variables. Let must be used in order for the variable to be reassigned so it can change. I  have now declared the variable using the let keyword and it now prints (34) in the console.
