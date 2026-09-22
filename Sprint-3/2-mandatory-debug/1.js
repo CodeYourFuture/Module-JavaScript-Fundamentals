2d81e8b22067888de1613aa7fc82cd8fb5de5b98
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-22 21:14:25 +0100

    Add explanation for sum function bug and unreachable code

--- Sprint-3/2-mandatory-debug/1.js
+++ Sprint-3/2-mandatory-debug/1.js
@@ -11,5 +11,8 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+// When the code was run it logged 'The sum of 10 and 32 is undefined'
+// The bug is in line 7
+// 'return' is automatically assigned a semi-colon if it is followed by a line break, making line 8 unreachable
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
