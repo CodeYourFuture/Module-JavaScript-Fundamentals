2fe022ada4f796a9e32f32cdcab0fb480a4cd2ee
shirinpanahian <sh.panahian@gmail.com>
2026-09-22 23:15:15 +0100

    fix the error

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -1,13 +1,21 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  function parameter and variable inside the function have same name.
 
 // call the function capitalise with a string input
+console.log(capitalise("message"));
 // interpret the error message and figure out why an error is occurring
-
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+/*Identifier 'str' has already been declared . 
+This mean that 'str' has already been declared as a function parameter,
+so we cannot declare another variable with the same name using 'let'.
+ */
 
 // =============> write your explanation here
+// The error occurs because 'str' is already declare as a function parameter.
+//The 'let str' inside the function tries to declare 'str' again in the same scope.
+//I changed the name of new variable to 'result' to fix the error.
+
 // =============> write your new code here
+function capitalise(str) {
+  let result = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return result;
+}
