0dafc76ae850765be1618083b6074087c13bcd59
Fatima Rouchi <fatima.aouil@hotmail.com>
2026-09-22 16:10:23 +0100

    complete ex. 1

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -5,3 +5,7 @@ count = count + 1;
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
 console.log(`${count}`);
+
+// line 3 is reassigning the value of count by changing it to count+1
+// and the = sign takes the value on the right and stores it into the variable on the left
+// in this case in line 3 the the variable is assigned a new value telling it to take the old one and add 1 to it.
