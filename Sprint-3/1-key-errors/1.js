e2a617b9f5fe5b5f87281e575ff57b2bc5dbd6ae
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-22 19:03:04 +0100

    Implement convertToPercentage function to calculate percentage from decimal input

--- Sprint-3/1-key-errors/1.js
+++ Sprint-3/1-key-errors/1.js
@@ -25,3 +25,8 @@
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber){
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+console.log(convertToPercentage(0.5));
