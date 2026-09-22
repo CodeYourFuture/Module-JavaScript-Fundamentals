3c5ca53bec74bfebaf7d88a0ddb4ed06549aff42
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-09-22 01:36:04 +0100

    updated the logged function

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -3,4 +3,4 @@ const twentyFourHourClockTime = "20:53";
 // The old version of the variables return a SyntaxError pointing that there is Invalid or unexpected token
 // For naming the variables we need to use the naming convention which involved not leading the variable name with a number
 
-console.log(twelveClockTime);
+console.log(twelveHourClockTime);
