4f42d5d21cc4f6099c31654b5afb8a05eea5313f
Diana <40689028+ausiejute@users.noreply.github.com>
2026-09-22 23:36:51 +0100

    Double check the answers

--- Sprint-2/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-2/3-mandatory-interpret/1-percentage-change.js
@@ -12,7 +12,7 @@ console.log(`The percentage change is ${percentageChange}`);
 // Read the code and then answer the questions below
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
-// 4, 5, 8
+// 4(2), 5(2), 10
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
 // It was a syntax error, it can be fixed by adding the missing part (1 of 2 parentheses)
 // c) Identify all the lines that are variable reassignment statements
