1234dae259c4cb2a650f7df6b927af1e92dc7ab7
Fatima Rouchi <fatima.aouil@hotmail.com>
2026-09-22 19:01:33 +0100

    fix error in 2js

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -3,3 +3,9 @@
 
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+
+/* The log is trying to use cityOfBirth before the variable exists.
+ JavaScript reads the code in order, so the variable must be declared before it is used.*/
+// I logged the message before creating the cityOfBirth variable, so JavaScript couldn't find it yet. I need to declare the variable before using it.
+const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
