fb93c79135c8b8a2b93762f827abeedcda98ed37
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-22 21:32:44 +0100

    task 2.js completed

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -1,20 +1,33 @@
 
 // Predict and explain first BEFORE you run any code...
+// Answer below:
+// The code will not work because the placeholder variable (3) should be an actual name like "num"
+
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
+
+
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// The error message explains the error in the function parameter (3), the function expects a name parameter instead of a number.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+
+function square(num) {
+    return num * num;
+}
+console.log(square(10))
+
 
 
