2fd1da46f8a7a3a43f97ee118f3d972ce8f69283
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-22 21:46:26 +0100

    Implement getLastDigit function to return the last digit of a number and update console log outputs

--- Sprint-3/2-mandatory-debug/2.js
+++ Sprint-3/2-mandatory-debug/2.js
@@ -28,5 +28,13 @@
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 
+function getLastDigit(n){
+  return n.toString().slice(-1)
+}
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
