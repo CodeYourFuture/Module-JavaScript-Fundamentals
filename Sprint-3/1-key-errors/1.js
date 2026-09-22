ec7939cb83c8e280c99ed5c9b1e930ca5977df29
shirinpanahian <sh.panahian@gmail.com>
2026-09-22 23:37:08 +0100

    change variable scope

--- Sprint-3/1-key-errors/1.js
+++ Sprint-3/1-key-errors/1.js
@@ -2,19 +2,27 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
-
+// If we run the program, I predict we will get an error.
+// The first problem is that 'decimalNumber' is already function parameter, but you try to declare another variable with the same name inside the function.
+//another problem is that 'decimalNumber' is a local variable inside the function
+// and we cannot access to it outside the function.
+//------------------------------------------------------
 // Try playing computer with the example to work out what is going on
+//SyntaxError: Identifier 'decimalNumber' has already been declared
+
+// =============> write your explanation here
+
+// The error occurs because 'decimalNumber' has already been declared
+// as a function parameter. we cannot declare it again with 'const'
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+const decimalNumber = 0.5;
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage(decimalNumber));
