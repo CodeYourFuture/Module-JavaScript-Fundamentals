5839e8d79e4d3fe622d00887412fe5e64d3e4841
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-22 22:28:25 +0100

    task mandatory-debug completed

--- Sprint-3/2-mandatory-debug/0.js
+++ Sprint-3/2-mandatory-debug/0.js
@@ -1,14 +1,25 @@
 // Predict and explain first...
+// Answer below:
+// The code needs a value inside the console.log function to multiply, but no value was useed to replace the placeholder parameters
+// It should be an error, no value to return
 
 // =============> write your prediction here
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+// console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// Answer below:
+// The code has two function parameters, it prints the parameters without an actual value. To correct this code the console.log has to be replaced with retun 
+// To return the value of the parameters when called
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b
+}
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 10)}`);
