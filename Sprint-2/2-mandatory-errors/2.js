609693d44a32fd9e16efce18fca8f2e81b820875
Fatima Rouchi <fatima.aouil@hotmail.com>
2026-09-22 19:03:30 +0100

    update 2.js

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,10 +1,7 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton";
-
-/* The log is trying to use cityOfBirth before the variable exists.
+/* The log was trying to use cityOfBirth before the variable exists.
  JavaScript reads the code in order, so the variable must be declared before it is used.*/
 // I logged the message before creating the cityOfBirth variable, so JavaScript couldn't find it yet. I need to declare the variable before using it.
 const cityOfBirth = "Bolton";
