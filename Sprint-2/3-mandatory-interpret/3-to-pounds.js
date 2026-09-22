770d48eceb2632d4ee74c3834d017c9717304dfd
Rhoda Ajiroba <ajiroba.rhoda@gmail.com>
2026-09-22 04:31:46 +0100

    Complete to pounds interpret exercise

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -2,13 +2,13 @@ const penceString = "399p";
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
-  penceString.length - 1
+  penceString.length - 1,
 );
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
 const pounds = paddedPenceNumberString.substring(
   0,
-  paddedPenceNumberString.length - 2
+  paddedPenceNumberString.length - 2,
 );
 
 const pence = paddedPenceNumberString
@@ -25,3 +25,8 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 2. Removes the trailing "p" from penceString, changing "399p" to "399".
+// 3. Pads the pence string with leading zeros so it has at least 3 characters.
+// 4. Extracts all digits except the last two to get the pounds part of the price.
+// 5. Extracts the last two digits to get the pence part and ensures it has two digits.
+// 6. Formats the pounds and pence as a price and prints it to the console.
