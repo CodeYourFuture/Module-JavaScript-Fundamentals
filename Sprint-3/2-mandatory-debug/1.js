7fc88536f5d509627e1ae1c33db79406bf534aa8
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-22 21:15:01 +0100

    Comment out the sum function and console log for debugging purposes

--- Sprint-3/2-mandatory-debug/1.js
+++ Sprint-3/2-mandatory-debug/1.js
@@ -3,16 +3,17 @@
 // The code seems like it would log 'The sum of 10 and 32 is 42' since function 'sum(10, 32)' seems to add two numbers 
 
 
-function sum(a, b) {
-  return;
-  a + b;
-}
+// function sum(a, b) {
+//   return;
+//   a + b;
+// }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
 // When the code was run it logged 'The sum of 10 and 32 is undefined'
 // The bug is in line 7
 // 'return' is automatically assigned a semi-colon if it is followed by a line break, making line 8 unreachable
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
