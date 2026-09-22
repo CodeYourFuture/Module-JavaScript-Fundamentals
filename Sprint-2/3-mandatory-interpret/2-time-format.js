e18f838326a405089b73f78a827f2e382156805f
Diana <40689028+ausiejute@users.noreply.github.com>
2026-09-22 01:02:56 +0100

    Completed mandatory interpretation tasks in the folder 3

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,18 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
-
+// 6
 // b) How many function calls are there?
-
+// 5
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+// The remainder operator calculates the remainder of movie in seconds (it does it by dividing the number by 60)
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
-
+// total minutes (in seconds) - remaining seconds = remaining seconds. Then converts the seconds into minutes
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
-
+// It represents how much of the movie is left to watch. Maybe something like remainderOfTheMovie
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// It won't work with all values. Most importantly, the value must be strictly numeric and positive
+
+console.log(result);
--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -2,13 +2,13 @@ const penceString = "399p";
 
 const penceStringWithoutTrailingP = penceString.substring(
   0,
-  penceString.length - 1
+  penceString.length - 1,
 );
 
 const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
 const pounds = paddedPenceNumberString.substring(
   0,
-  paddedPenceNumberString.length - 2
+  paddedPenceNumberString.length - 2,
 );
 
 const pence = paddedPenceNumberString
@@ -24,4 +24,12 @@ console.log(`£${pounds}.${pence}`);
 // Try and describe the purpose / rationale behind each step
 
 // To begin, we can start with
-// 1. const penceString = "399p": initialises a string variable with the value "399p"
+// 1. const penceString = "399p": initializes a string variable with the value "399p"
+// 3-6. const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
+// : initializes a variable, the value of which is turned to numerical by removing the letter "p".
+// 8. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+// Here the padstart function turns "399" string into 399 number (it could add some zeroes in front, but here it serves as a converter)
+// 9-12. const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
+// Here the goal is to initialize a variable of Pounds (with the value of 3), by splitting it from 99, and it's done using the substring method
+// 14-16. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
+// Here the pence variable is introduced with the value of 99 and it's done by taking the 399 and removing 3 using substring and padEnd methods.
