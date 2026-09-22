409baf985ef162cb8b7f49c4c11ffe43ee0c46a9
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-22 19:27:50 +0100

    Add explanation for SyntaxError in square function parameter

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -14,6 +14,8 @@ function square(3) {
 // SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// In the 'function square(3)' JavaScript parser expected a valid parameter name it could use as a variable
+// Since '3' is a number literal, not a valid identifier, the parser cannot understand it and throws a syntax error.
 
 // Finally, correct the code to fix the problem
 
