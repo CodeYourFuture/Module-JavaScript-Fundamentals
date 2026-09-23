f1cf9993253918e0efae922504b6212013c3d4c9
Diana <40689028+ausiejute@users.noreply.github.com>
2026-09-23 17:24:34 +0100

    correct answers to b, c, d questions

--- Sprint-2/3-mandatory-interpret/1-percentage-change.js
+++ Sprint-2/3-mandatory-interpret/1-percentage-change.js
@@ -14,10 +14,10 @@ console.log(`The percentage change is ${percentageChange}`);
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 // 4(2), 5(2), 10
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
-// It was a syntax error, it can be fixed by adding the missing part (1 of 2 parentheses)
+// It was a syntax error, it can be fixed by adding the missing part (1 of 2 parentheses). Comma between 2 arguments was missing.
 // c) Identify all the lines that are variable reassignment statements
-// 4, 5, 7, 8
+// 4, 5
 // d) Identify all the lines that are variable declarations
-// 1, 2
+// 1, 2, 7, 8
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
 // It turns/converts a string into a number by removing the comma and quotation marks (since they are non-number values)
