453e08632b727a126dcf6f7d29b319f4e39c0903
gaireprakash20-ops <gaireprakash20@gmail.com>
2026-09-23 23:42:32 +0100

    put let varible except const

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -1,7 +1,10 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+const age =33;
 age = age + 1;
 
 /* In this case age is not const means variable is not reassigned so that,
- we throws a TypeError: Assignment to constant variable.2
+ /*we throws a TypeError: Assignment to constant variable*/  
+ //I try  by let where console.log(age) shows Running] node "c:\Users\Asus\Desktop\code your future\Module-Onboarding\Module-JavaScript-Fundamentals\Sprint-2\Sprint-2\2-mandatory-errors\1.js"
+//34.
+
