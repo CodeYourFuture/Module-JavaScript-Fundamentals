f3b91ebc9d3823219650f42184f7938bbf884636
shirinpanahian <sh.panahian@gmail.com>
2026-09-23 23:09:13 +0100

    moving return to correct code

--- Sprint-3/2-mandatory-debug/1.js
+++ Sprint-3/2-mandatory-debug/1.js
@@ -1,13 +1,21 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> I think the position of return inside the function is wrong
+//probably it is show nothing
 
-function sum(a, b) {
+/*function sum(a, b) {
   return;
   a + b;
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`); */
 
 // =============> write your explanation here
+// After running code it is show "The sum of 10 and 32 is undefined"
+// because return has no value so it return undefined.
+
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
