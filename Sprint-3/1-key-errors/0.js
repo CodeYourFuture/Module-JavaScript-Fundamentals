638af555ba7bbf4906ff793738195ab887e6f066
Roger Lee <lee.roger.fn@gmail.com>
2026-09-23 10:51:06 +0100

    answered 1-key-errors 0.js

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -1,13 +1,26 @@
 // Predict and explain first...
 //  =============> write your prediction here
+//A: when user input the string. First index will be capitalised.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+//A: SyntaxError: Identifier 'str' has already been declared
 
+//function capitalise(str) {
+//  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//  return str;
+//}
+
+
+// =============> write your explanation here
+//"str" has been declared in function. In the local scope of original code, str declared again.
+//It caused the problem of redeclaration. Delete the "let" in local scope
+// =============> write your new code here
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+console.log(capitalise('roger'))
+
+
