227b3eb56529ca794cbe4b3517def19455e9e636
Roger Lee <lee.roger.fn@gmail.com>
2026-09-23 11:43:41 +0100

    answered 2-mandatory-debugs 0.js

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -1,4 +1,3 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
@@ -20,7 +19,7 @@
 
 // =============> write your new code here
 function square(num) {
-    return num * num;
+  return num * num;
 }
 
-console.log(square(3))
+console.log(square(3));
