f267ff6de19e11619ee9c9c0d914536825436e3e
Diana <40689028+ausiejute@users.noreply.github.com>
2026-09-23 18:10:13 +0100

    Fix line 29, add a step for line 18

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -26,7 +26,7 @@ console.log(`£${pounds}.${pence}`);
 // To begin, we can start with
 // 1. const penceString = "399p": initializes a string variable with the value "399p"
 // 3-6. const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
-// : initializes a variable, the value of which is turned to numerical by removing the letter "p".
+// : initializes a variable, removes the letter 'p'(it's still a string)
 // 8. const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
 // Here the padstart function turns "399" string into 399 number (it could add some zeroes in front, but here it serves as a converter)
 // Correct answer: Ensures the string is at least 3 characters long by adding leading zeros if needed. With "399", it stays "399". But if the input were "5p", this would become "005".
@@ -34,3 +34,4 @@ console.log(`£${pounds}.${pence}`);
 // Here the goal is to initialize a variable of Pounds (with the value of 3), by splitting it from 99, and it's done using the substring method
 // 14-16. const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
 // Here the pence variable is introduced with the value of 99 and it's done by taking the 399 and removing 3 using substring and padEnd methods.
+// 18. Here we join pounds and pence together and print out the result - the price.
