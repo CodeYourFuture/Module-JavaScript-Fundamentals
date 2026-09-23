76ef1226eec9deac54911a856fd8eb34c92cfa1a
Roger Lee <lee.roger.fn@gmail.com>
2026-09-23 10:51:06 +0100

    answered 1-key-errors 0.js

---
+++ Sprint-3/prep/passwordCheckerFunction.js
@@ -0,0 +1,9 @@
+const password = "secretword123";
+
+function checkPassword(userInput){
+
+  const response = userInput === password;
+
+  return response;
+
+  console.log(checkPassword(secretword123))
