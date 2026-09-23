cd6e897c599d0915820dcc56e7623cef34e8a019
Tyler-leigh Ross <rossleighit@gmail.com>
2026-09-23 11:12:30 +0200

    Predicted, explained and fixed code errors

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -4,17 +4,25 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// Error because 3 is a value, functions take parameters(like num). 
+// It wont work 3 is a number literal and not a name/identifier.
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// The code was expecting the parameter name but found the number literal =3
+// This is why we get the syntax error.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+    return num * num;
+}
 
-
+console.log(square(3));
