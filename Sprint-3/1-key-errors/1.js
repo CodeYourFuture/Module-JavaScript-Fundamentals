dcc7246e3c3b908417022d6bf2b407e6cf1b5c6f
Roger Lee <lee.roger.fn@gmail.com>
2026-09-23 11:06:51 +0100

    answered 1-key-errors 1.js

--- Sprint-3/1-key-errors/1.js
+++ Sprint-3/1-key-errors/1.js
@@ -2,19 +2,31 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
+// A: There have the problem of redeclaration of the statement of decimalNumber. Local Scope should return to decimalNumber instead.
+// To print the code, the code should be console.log(decimalNumber)
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+//function convertToPercentage(decimalNumber) {
+//  const decimalNumber = 0.5;
+//  const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
+//  return percentage;
 
-console.log(decimalNumber);
+//console.log(decimalNumber);
 
 // =============> write your explanation here
+//A: In the local scope, remove "const decimalNumber = 0.5". for the second line of local scope, 'const percentage' should be change to
+//'decimalNumber' to avoid redeclaration and refer the correct variable. and return the function to 'decimalNumber'
+//finally console.log(convertToPercentage(0.5)) should be used to print the function.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  decimalNumber = `${decimalNumber * 100}%`;
+
+  return decimalNumber;
+}
+
+console.log(convertToPercentage(0.5));
