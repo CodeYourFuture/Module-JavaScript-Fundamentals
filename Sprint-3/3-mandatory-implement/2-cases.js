25fdf9bfbcbb3fa602ce3dcd639384c23673c222
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-23 17:21:46 +0100

    cases task completed

--- Sprint-3/3-mandatory-implement/2-cases.js
+++ Sprint-3/3-mandatory-implement/2-cases.js
@@ -14,3 +14,9 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function upperCase(str) {
+    returnUpperCase = str.toUpperCase().replaceAll(" ", "_")
+    return returnUpperCase
+}
+console.log(upperCase("my name is mathew"))
