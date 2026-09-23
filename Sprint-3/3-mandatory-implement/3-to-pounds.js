93a453c5abe9f6bd30f755e989ea0a36dd7c6c91
Precious Moses <mosesjeffrey74@gmail.com>
2026-09-23 17:02:40 +0100

    Add additional test cases for toPounds function

--- Sprint-3/3-mandatory-implement/3-to-pounds.js
+++ Sprint-3/3-mandatory-implement/3-to-pounds.js
@@ -6,11 +6,16 @@
 // You should call this function a number of times to check it works for different inputs
 
 function toPounds(penceString){
-    const penceStringWithoutTrailingP = penceString.subString(0, penceString.length - 1);
+    const penceStringWithoutTrailingP = penceString.substring(0, penceString.length - 1);
     const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
-    const pounds = paddedPenceNumberString.subString(0, paddedPenceNumberString.length - 2);
+    const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
     const pence = paddedPenceNumberString.substring(paddedPenceNumberString.length - 2).padEnd(2, "0");
 
     return `£${pounds}.${pence}`;
 }
 
+console.log(toPounds("77770p"));
+console.log(toPounds("2570p"));
+console.log(toPounds("399p"));
+console.log(toPounds("77p"));
+console.log(toPounds("0p"));
