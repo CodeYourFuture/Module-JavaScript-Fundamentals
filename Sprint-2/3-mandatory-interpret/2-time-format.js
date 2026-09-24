294f0f50021832571d57e2e1012e4ab7950a96a4
gaireprakash20-ops <gaireprakash20@gmail.com>
2026-09-24 12:56:28 +0100

    explain the percentage meaning

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -13,22 +13,23 @@ console.log(result);
 
 // a) How many variable declarations are there in this program?
 /* line 1 const movieLength  line 3 const remainingSeconds line 4 const totalMinutes
-   line 6 const remainingMinutes line 7 const totalHours  line 9 const result
+   line 6 const remainingMinutes line 7 const totalHours  line 9 const result*/
 
 // b) How many function calls are there?
 /* there are one function 
-      console.log(result);
+      console.log(result);*/
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
-/*The remainder (%) operator returns the remainder left over when one operand is divided by a second operand. It always takes the sign of the dividend.
+/*The remainder (%) operator returns the remainder left over when one operand is divided by a second operand. It always takes the sign of the dividend*/
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
-/* This expression convert movies time second into minutes dividend by 60 second.
+/* This expression convert movies time second into minutes dividend by 60 second*/
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
 /* The variable represent the movieLength in second,minutes and hours. 
-    We can change this name as movieTime.
+    We can change this name as movieTime*/
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
-/* In this section we can only use the positive natural numbers  but if we put numbers that divide by 60 without a remainder get the exact time like 10, 20 50.
+/* In this section we can only use the positive natural numbers  but if we put numbers that divide by 60 without a remainder get the exact time like 10, 20 50 */
+/*"Percent (%) is a way to describe the full amount of anything by imagining it split into 100 equal sections.*/
