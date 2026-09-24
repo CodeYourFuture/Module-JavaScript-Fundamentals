336821f4da0566efd9842411f0f02daf7da5d2fe
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-24 20:26:51 +0100

    Fix variable declaration in upperCase function

--- Sprint-3/3-mandatory-implement/2-cases.js
+++ Sprint-3/3-mandatory-implement/2-cases.js
@@ -16,7 +16,7 @@
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 
 function upperCase(str) {
-    returnUpperCase = str.toUpperCase().replaceAll(" ", "_")
+    const returnUpperCase = str.toUpperCase().replaceAll(" ", "_")
     return returnUpperCase
 }
 console.log(upperCase("what is your name mr man? "))
