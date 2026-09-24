d8b60b7b558c58a13beab38adca0977f4e120a32
Frumentius-Rev <tesfaybh@gmail.com>
2026-09-24 14:11:42 +0100

    Complete uppercase conversion

--- Sprint-3/3-mandatory-implement/2-cases.js
+++ Sprint-3/3-mandatory-implement/2-cases.js
@@ -14,3 +14,10 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function toUpper(input) {
+    return input.replaceAll(" ", "_").toUpperCase();
+
+}
+
+console.log(toUpper("alpha and omega"));
