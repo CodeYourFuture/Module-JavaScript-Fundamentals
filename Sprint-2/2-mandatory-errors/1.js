2d3769c4cad85007df07717b00e5b9a8e73d6c7e
anitahy73 <anita.hy73@gmail.com>
2026-09-24 22:22:01 +0100

    Fix TypeError by changing const to let for age
    Corrected the reassignment of a constant variable in the code.

--- Sprint-2/2-mandatory-errors/1.js
+++ Sprint-2/2-mandatory-errors/1.js
@@ -1,5 +1,5 @@
 // trying to create an age variable and then reassign the value by 1
-
+// the vatriable age was declared as a const variable and trying to assigne another value caused a TypeError: Assignment to constant variable.
 let age = 33;
 age = age + 1;
 console.log(age)
