c32753d15a889eac7d1290aaa80d5eb5d82b32cd
gaireprakash20-ops <gaireprakash20@gmail.com>
2026-09-24 01:22:17 +0100

    placed the number at the end

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -1,3 +1,6 @@
-const 12HourClockTime = "8:53pm";
-const 24hourClockTime = "20:53";
-/*variable is not start with number show syntaxerror.
+const HourClockTime12 = "8:53pm";
+const hourClockTime24 = "20:53";
+
+
+/*variable is not start with number show syntaxerror.*/
+// i just move the number at the end.
