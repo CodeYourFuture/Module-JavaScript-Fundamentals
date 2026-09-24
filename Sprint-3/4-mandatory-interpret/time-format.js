706a763d8b69a14e0043b18d547d13fe1d1ebaec
Frumentius-Rev <tesfaybh@gmail.com>
2026-09-24 21:24:04 +0100

    Complete function tracing exercise

--- Sprint-3/4-mandatory-interpret/time-format.js
+++ Sprint-3/4-mandatory-interpret/time-format.js
@@ -14,6 +14,7 @@ function formatTimeDisplay(seconds) {
 
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
+console.log(formatTimeDisplay(61));
 
 // You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
 // to help you answer these questions
@@ -27,6 +28,7 @@ function formatTimeDisplay(seconds) {
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
+// 3
 
 // c) What is the return value of pad when it is called for the first time?
 // =============> write your answer here
