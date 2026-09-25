63539e9fcfcd6b16d18741c8e6bbf71b040a33e6
Vaidah <veesimz@gmail.com>
2026-09-25 17:14:23 +0200

    change console.log to return in sprint 3 key error 3

--- Sprint-3/2-mandatory-debug/0.js
+++ Sprint-3/2-mandatory-debug/0.js
@@ -1,14 +1,15 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// Prediction:I think it will print 320, but the template string might not show the right result, but will return nothing.
 
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
+// because console.log will print whatever it is given . 
+// the function has no return statement, so when we run the code it will say undefined because there is nothing to give back.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
