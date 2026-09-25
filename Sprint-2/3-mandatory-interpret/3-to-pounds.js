5d3ef361fa04a766356c245c61e64482f9f99b79
Mukhethwa <ravele.mk@gmail.com>
2026-09-26 01:56:58 +0200

    interpret3 exercise complete

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -25,3 +25,10 @@ console.log(`£${pounds}.${pence}`);
 
 // To begin, we can start with
 // 1. const penceString = "399p": initialises a string variable with the value "399p"
+//3. we are defining  a new variable without the currency at the end that takes a sub string function  from the penceString
+//4. the substring starts from index 0
+//5.until lenth of string -1 brfore last character
+//8.  adds 0 char to the beginning of string until length is 3
+//9-11 Extracts a substring starting from the beginning of the string and stopping exactly 2 characters from the end
+//14-16 Extracts only the last two characters of the string. The .padEnd(2, "0") appends zeros to the right if the resulting string is somehow shorter than 2 characters
+//18 Uses a template literal to insert the pounds and pence variables into a new string, formatted with a pound symbol and a decimal point, and prints it to the console
