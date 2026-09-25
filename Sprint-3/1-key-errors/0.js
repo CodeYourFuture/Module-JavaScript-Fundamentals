a6c7eb16d670cfa99d539a4e71cb6bf20b02de00
MattCarolus10 <mattcarolus10@gmail.com>
2026-09-25 18:00:21 +0100

    restore Sprint-3 to upstream

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -1,23 +1,13 @@
 // Predict and explain first...
-//  ======> write your prediction here: I predict that the code should cut out, and make the first letter of the string an uppercase letter,
-//  then add every letters that comes after
+//  =============> write your prediction here
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-// function capitalise(str) {
-//  let str= `${str[0].toUpperCase()}${str.slice(1)}`
-//   return str;
-// }
-
-
-// =============> write your explanation here: The error read "SyntaxError: Identifier 'str' has already been declared",
-//  this is as a result of having two variables with same name. The error occoured in this code because the variable "str" has been decleared twice
-
-// =============> write your new code here
-
 function capitalise(str) {
- let str2= `${str[0].toUpperCase()}${str.slice(1)}`
-  return str2;
+  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
 }
-console.log(capitalise("money"))
+
+// =============> write your explanation here
+// =============> write your new code here
