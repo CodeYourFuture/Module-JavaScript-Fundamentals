bbe2a4e8b8f2a93d0bc6497ae979fb88705ec376
Vaidah <veesimz@gmail.com>
2026-09-25 19:05:07 +0200

    implement calculateBMI

--- Sprint-3/3-mandatory-implement/1-bmi.js
+++ Sprint-3/3-mandatory-implement/1-bmi.js
@@ -15,5 +15,7 @@
 // It should return a string of their Body Mass Index to 1 decimal place
 
 function calculateBMI(weight, height) {
-  // return the BMI of someone based off their weight and height
+ return (weight / (height*height)). toFixed(1);
 }
+
+console.log(calculateBMI(70, 1.73));
