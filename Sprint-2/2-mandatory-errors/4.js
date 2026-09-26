e4b6cae2877b76cbf557d2f8ab500f9cd3a883bb
Vaidah <veesimz@gmail.com>
2026-09-26 12:29:24 +0200

    explain variable rename in sprint 2 error 4

--- Sprint-2/2-mandatory-errors/4.js
+++ Sprint-2/2-mandatory-errors/4.js
@@ -3,3 +3,9 @@ const twentyFourHourClockTime = "20:53";
 
 console.log(twelveHourClockTime);
 console.log(twentyFourHourClockTime);
+
+// Original code used `12HourClockTime` and `24hourClockTime` as variable names.
+// but these start with numbers, which JavaScript doesn't allow 
+// variable names must start with a letter, underscore, or dollar sign.
+// So I renamed them to `twelveHourClockTime` and `twentyFourHourClockTime`.
+// This avoids the SyntaxError and makes the code run.
