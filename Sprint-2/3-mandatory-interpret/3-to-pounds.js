fbb60c9b36ea7f8a2ad146a4ed418ae1edf20f59
anitahy73 <anita.hy73@gmail.com>
2026-09-26 16:43:15 +0100

    Refine comments in 3-to-pounds.js
    Updated comments for clarity and consistency.

--- Sprint-2/3-mandatory-interpret/3-to-pounds.js
+++ Sprint-2/3-mandatory-interpret/3-to-pounds.js
@@ -25,12 +25,16 @@ console.log(`£${pounds}.${pence}`);
 //this function penceString.length-1 removes the last p from string so it values 3 because it contains 4 characters minues 1.
 //penceString.substring(0, 3) and it takes the characters from position 0 up to, but not including, position 3.
 //const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");This uses padStart() to make sure the string has at least 3 characters.
+//padStart() adds characters to the begining of a string to make it 3 character long.
+
 //const pounds = paddedPenceNumberString.substring(0, paddedPenceNumberString.length - 2);
+// we remove the last two characters and stores the remaining part of the string as pounds.
 //This line extracts the pounds part.
 //line14:penceString = "399p"
 
 //paddedPenceNumberString is "399".
-//The substring() takes the last two characters, so:"399" → "99"
+//The substring() takes the last two characters as pence., so:"399" → "99"
 //Then .padEnd(2, "0") checks the string is at least 2 characters long. If it already has 2 characters, like "99", without changing the rest.
-//padEnd(2, "0") adds "0" to the end of the string until it reaches a length of 2 and finally line 18 prints the result which is £3.99
+//padEnd(2, "0") adds "0" to the end of the string until it reaches a length of 2.
+//finally line 18 combines pound and pence which is £3.99
 
