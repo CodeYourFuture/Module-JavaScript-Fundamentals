a92c307591613d2d99bad846a1fc519222376910
gaireprakash20-ops <gaireprakash20@gmail.com>
2026-09-26 14:01:59 +0100

    test the two different negative and decimal

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -5,4 +5,4 @@ console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
 
 
-/*we need to put declare variables(const cityOfBirth = "Bolton";) in first line after using expression console.log.
+/*we need to put declare variables(const cityOfBirth = "Bolton";) in first line after using expression console.log.*/
