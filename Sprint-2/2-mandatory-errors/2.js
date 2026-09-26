7e294c80571faa398cfa41df9e2d9b9fcdf3ce38
anitahy73 <anita.hy73@gmail.com>
2026-09-26 16:05:35 +0100

    Fix ReferenceError by declaring cityOfBirth first
    Fixed the order of variable declaration and usage for cityOfBirth.

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,5 +1,5 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error?
+// what's the error?in the original code,line 4 creates it and line 3 uses the createOfBirth,Then I changed console.log to the next line because we need to declare the variable before using it.
 //ReferenceError: Cannot access 'cityOfBirth' before initialization
 
 const cityOfBirth = "Bolton";
