1794dd4920689b6462327a48183d311836730edb
Vaidah <veesimz@gmail.com>
2026-09-26 11:08:40 +0200

    add both comment styles per review in sprint 2 error 0

--- Sprint-2/2-mandatory-errors/0.js
+++ Sprint-2/2-mandatory-errors/0.js
@@ -1,4 +1,5 @@
 //This is just an instruction for the first activity - but it is just for human consumption
 //We don't want the computer to run these 2 lines - how can we solve this problem?
 
-   //answer: by adding the 2 forward slashes before every line
+   //answer: 1.by adding the 2 forward slashes before every line for single-line comment.
+   //        2.If it is a multi-line comment you wrap the code in a slash-star and slash-star symbols
